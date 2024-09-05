bd20f419dc4eab68fc3bc09a78a84e8dbcfb0333
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-09-05 09:47:58 +0530

    MissigObservation_2028 template created

---
+++ MissingObservation_2028.cpp
@@ -0,0 +1,16 @@
+class MissingObservation_2028
+{
+private:
+    /* data */
+public:
+    MissingObservation_2028(/* args */);
+    ~MissingObservation_2028();
+};
+
+MissingObservation_2028::MissingObservation_2028(/* args */)
+{
+}
+
+MissingObservation_2028::~MissingObservation_2028()
+{
+}
