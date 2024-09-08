17f321d6b1626cc515b7dbbaeb5ac97bcb32ff48
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-09-08 17:35:42 +0530

    SplitLinkedList Template is Ready

---
+++ SplitLinkedList_725.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+class SplitLinkedList_725
+{
+public:
+    vector<ListNode *> splitListToParts(ListNode *head, int k)
+    {
+    }
+};
+int main()
+{
+
+    return 0;
+}
