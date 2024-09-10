7376696e15d95bb80c05d47c7211ca7d703b4d5b
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-09-10 09:47:43 +0530

    Comments Added

--- InsertGCDInLinkedList_2807.cpp
+++ InsertGCDInLinkedList_2807.cpp
@@ -28,18 +28,21 @@ public:
     {
         ListNode *temp = head;
 
+        // Traverse the linked list and insert GCD nodes
         while (temp != nullptr && temp->next != nullptr)
         {
             int gcd = getGCD(temp->val, temp->next->val);
 
+            // Insert the GCD node between the current node and the next node
             ListNode *gcd_node = new ListNode(gcd);
             gcd_node->next = temp->next;
             temp->next = gcd_node;
 
+            // Move temp two steps forward
             temp = gcd_node->next;
         }
 
-        return head;
+        return head; // Return the modified list starting from head
     }
 };
 
@@ -48,10 +51,10 @@ int main()
     InsertGCDInLinkedList_2807 in;
 
     // Creating the linked list: 10 -> 6 -> 10 -> 3
-    ListNode *head = new ListNode(7);
-    // head->next = new ListNode(6);
-    // head->next->next = new ListNode(10);
-    // head->next->next->next = new ListNode(3);
+    ListNode *head = new ListNode(18);
+    head->next = new ListNode(6);
+    head->next->next = new ListNode(10);
+    head->next->next->next = new ListNode(3);
 
     // Inserting GCD nodes
     ListNode *result = in.insertGreatestCommonDivisors(head);
@@ -66,9 +69,3 @@ int main()
 
     return 0;
 }
-static const int hansil = []() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    return 0;
-}();
