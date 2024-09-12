80edfe8cc6df0ffb507bf92a6673fddf0dc031a8
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-09-12 10:14:02 +0530

    CountTheNumberOfConsistentString_1684 template ready

---
+++ CountTheNumberOfConsistentString_1684.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class CountTheNumberOfConsistentString_1684
+{
+private:
+    /* data */
+public:
+    int countConsistentStrings(string allowed, vector<string> &words);
+};
+int CountTheNumberOfConsistentString_1684::countConsistentStrings(string allowed, vector<string> &words)
+{
+    for (int i = 0; i < words.size(); i++)
+    {
+
+    }
+
+}
