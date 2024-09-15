60a44956fc55f4b04f7c307d4e8b988beb4453e6
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-09-15 17:37:16 +0530

    LongestSubStringContainVowelsInEvenCount_1371 Done

--- LongestSubStringContainVowelsInEvenCount_1371.cpp
+++ LongestSubStringContainVowelsInEvenCount_1371.cpp
@@ -1,18 +1,75 @@
-#include<iostream>
+#include <iostream>
+#include <unordered_map>
+#include <vector>
 using namespace std;
+
 class LongestSubStringContainVowelsInEvenCount_1371
 {
-private:
-    /* data */
 public:
-    LongestSubStringContainVowelsInEvenCount_1371(/* args */);
-    ~LongestSubStringContainVowelsInEvenCount_1371();
+    int findTheLongestSubstring(string s)
+    {
+        int mask = 0;
+        unordered_map<int, int> mask_map;
+        mask_map[0] = -1;
+        int max_len = 0;
+
+        for (int i = 0; i < s.length(); i++)
+        {
+            switch (s[i])
+            {
+            case 'a':
+                mask ^= 1 << 0;
+                break;
+            case 'e':
+                mask ^= 1 << 1;
+                break;
+            case 'i':
+                mask ^= 1 << 2;
+                break;
+            case 'o':
+                mask ^= 1 << 3;
+                break;
+            case 'u':
+                mask ^= 1 << 4;
+                break;
+            default:
+                break;
+            }
+
+            if (mask_map.find(mask) != mask_map.end())
+            {
+                max_len = max(max_len, i - mask_map[mask]);
+            }
+            else
+            {
+                mask_map[mask] = i;
+            }
+        }
+        return max_len;
+    }
 };
 
-LongestSubStringContainVowelsInEvenCount_1371::LongestSubStringContainVowelsInEvenCount_1371(/* args */)
+int main()
 {
+    LongestSubStringContainVowelsInEvenCount_1371 l1;
+    string s = "eleetminicoworoep";
+    cout << "The length of the longest substring with vowels in even count is: "
+         << l1.findTheLongestSubstring(s) << endl;
+    return 0;
 }
 
-LongestSubStringContainVowelsInEvenCount_1371::~LongestSubStringContainVowelsInEvenCount_1371()
-{
-}
+static const int hansil = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
+
+/*
+How the Length is Calculated:
+The length of the valid substring is always calculated as:
+
+cpp
+Copy code
+current_index - previous_index_of_same_mask
+*/
