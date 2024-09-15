f06a82660625dffff5f97addc9bf241cee1d3653
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-09-15 16:38:50 +0530

    1371 template is ready

---
+++ LongestSubStringContainVowelsInEvenCount_1371.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+class LongestSubStringContainVowelsInEvenCount_1371
+{
+private:
+    /* data */
+public:
+    LongestSubStringContainVowelsInEvenCount_1371(/* args */);
+    ~LongestSubStringContainVowelsInEvenCount_1371();
+};
+
+LongestSubStringContainVowelsInEvenCount_1371::LongestSubStringContainVowelsInEvenCount_1371(/* args */)
+{
+}
+
+LongestSubStringContainVowelsInEvenCount_1371::~LongestSubStringContainVowelsInEvenCount_1371()
+{
+}
