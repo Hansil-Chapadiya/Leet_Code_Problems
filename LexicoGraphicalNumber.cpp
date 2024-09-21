43886b9b77ef42ce404d54f0ef42814369ce2c87
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-09-21 13:21:18 +0530

    LexicoGraphical Number template is ready

---
+++ LexicoGraphicalNumber.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+class LexicoGraphicalNumber
+{
+private:
+    /* data */
+public:
+    std::vector<int> lexicalOrder(int n)
+    {
+    }
+};
+int main()
+{
+}
