88828147837543616845678cc7af955d682f792f
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-09-23 18:15:31 +0530

    ExtraCharInString_2707.cpp template is ready

---
+++ ExtraCharInString_2707.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <string>
+class ExtraCharInString_2707
+{
+private:
+    /* data */
+public:
+    int minExtraChar(std::string s, std::vector<std::string> &dictionary)
+    {
+        for (int i = 0; i < dictionary.size(); i++)
+        {
+            /* code */
+        }
+    }
+};
+int main()
+{
+    return 0;
+}
