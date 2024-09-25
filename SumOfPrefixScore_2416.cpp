d55080242147f8739f3d8086d99f1fd52dea2bd6
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-09-25 17:57:06 +0530

    SumOFPrefixScore_2416 template is ready

---
+++ SumOfPrefixScore_2416.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+class SumOfPrefixScore_2416
+{
+private:
+    /* data */
+public:
+    SumOfPrefixScore_2416(/* args */);
+    ~SumOfPrefixScore_2416();
+};
+
+SumOfPrefixScore_2416::SumOfPrefixScore_2416(/* args */)
+{
+}
+
+SumOfPrefixScore_2416::~SumOfPrefixScore_2416()
+{
+}
