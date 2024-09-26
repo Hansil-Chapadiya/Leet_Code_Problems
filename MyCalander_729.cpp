c55de3314582478b1914659d4d800a542b23a0fb
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-09-26 19:26:39 +0530

    My_calander_I_729.cpp template is ready

---
+++ MyCalander_729.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+class MyCalander_729
+{
+private:
+    /* data */
+public:
+    MyCalander_729(/* args */);
+    ~MyCalander_729();
+};
+
+MyCalander_729::MyCalander_729(/* args */)
+{
+}
+
+MyCalander_729::~MyCalander_729()
+{
+}
+int main()
+{
+    return 0;
+}
