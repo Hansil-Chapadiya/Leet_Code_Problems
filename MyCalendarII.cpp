ba9a4bb86c0b28a330be7099e6e4b0699213cc37
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-09-27 16:27:37 +0530

    MyCalanderII template is ready

---
+++ MyCalendarII.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+class MyCalendarII
+{
+private:
+    /* data */
+public:
+    MyCalendarII()
+    {
+    }
+
+    bool book(int start, int end)
+    {
+    }
+    ~MyCalendarII();
+};
+int main()
+{
+    return 0;
+}
