d667c957d32de24b0ff38454d62c785e75ec9c52
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-09-28 09:07:47 +0530

    Circular queue template ready

---
+++ DesignCircularDequeue_641.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+class DesignCircularDequeue_641
+{
+private:
+    /* data */
+public:
+    DesignCircularDequeue_641(int k) {
+
+    }
+
+    bool insertFront(int value) {
+
+    }
+
+    bool insertLast(int value) {
+
+    }
+
+    bool deleteFront() {
+
+    }
+
+    bool deleteLast() {
+
+    }
+
+    int getFront() {
+
+    }
+
+    int getRear() {
+
+    }
+
+    bool isEmpty() {
+
+    }
+
+    bool isFull() {
+
+    }
+};
+int main()
+{
+    return 0;
+
+}
