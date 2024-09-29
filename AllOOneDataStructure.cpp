1eeed44a24bb074b3b8d0497ef608017adcfab20
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-09-29 18:06:05 +0530

    AllOOneDataStructure.cpp template is ready

---
+++ AllOOneDataStructure.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+class AllOOneDataStructure
+{
+private:
+    /* data */
+public:
+    AllOOneDataStructure(/* args */);
+    ~AllOOneDataStructure();
+};
+
+AllOOneDataStructure::AllOOneDataStructure(/* args */)
+{
+}
+
+AllOOneDataStructure::~AllOOneDataStructure()
+{
+}
+int main()
+{
+    return 0;
+}
