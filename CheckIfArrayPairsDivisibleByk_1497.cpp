fd4ea4e080cfcdecd8dc0735b08631e4d5c5cb7a
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-10-01 21:51:18 +0530

    CheckIfArrayPairsDivisibleByk_1497.cpp done

--- CheckIfArrayPairsDivisibleByk_1497.cpp
+++ CheckIfArrayPairsDivisibleByk_1497.cpp
@@ -3,10 +3,8 @@
 #include <unordered_map>
 
 /* Produce wrong result */
-class CheckIfArrayPairsDivisibleByk_1497
+/* class CheckIfArrayPairsDivisibleByk_1497
 {
-private:
-    /* data */
 public:
     bool canArrange(std::vector<int> &arr, int k)
     {
@@ -31,12 +29,64 @@ public:
             return false;
         }
     }
+}; */
+
+// int main()
+// {
+//     CheckIfArrayPairsDivisibleByk_1497 C1;
+//     std::vector<int> arr = {1,2,3,4,5,10,6,7,8,9};
+//     std::cout << C1.canArrange(arr, 5);
+//     return 0;
+// }
+
+class CheckIfArrayPairsDivisibleByk_1497
+{
+public:
+    bool canArrange(std::vector<int> &arr, int k)
+    {
+        std::unordered_map<int, int> rem_cnt;
+
+        // Count the frequency of remainders
+        for (int num : arr)
+        {
+            int rem = ((num % k) + k) % k; // Handle negative remainders
+            rem_cnt[rem]++;
+        }
+
+        // Check for valid pairs
+        for (auto &val : rem_cnt)
+        {
+            int rem = val.first;
+            int count = val.second;
+
+            // For remainder 0, the count must be even to form pairs
+            if (rem == 0)
+            {
+                if (count % 2 != 0)
+                    return false;
+            }
+            else
+            {
+                // For any other remainder `r`, its count must match the count of `k - r`
+                if (rem_cnt[rem] != rem_cnt[k - rem])
+                    return false;
+            }
+        }
+
+        return true;
+    }
 };
 
 int main()
 {
     CheckIfArrayPairsDivisibleByk_1497 C1;
-    std::vector<int> arr = {1,2,3,4,5,10,6,7,8,9};
-    std::cout << C1.canArrange(arr, 5);
+    std::vector<int> arr = {75,5,-5,75,-2,-3,88,10,10,87};
+    std::cout << (C1.canArrange(arr, 85) ? "True" : "False") << std::endl;
     return 0;
 }
+static const int hansil = []() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return 0;
+}();
