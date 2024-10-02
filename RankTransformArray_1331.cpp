79a41b18ba714295ecc5207960a16059e0c1a32a
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-10-02 12:56:29 +0530

    RankTransformArray_1331.cpp

--- RankTransformArray_1331.cpp
+++ RankTransformArray_1331.cpp
@@ -1,40 +1,59 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 #include <unordered_map>
+#include <algorithm>
+
 class RankTransformArray_1331
 {
-private:
-    /* data */
 public:
     std::vector<int> arrayRankTransform(std::vector<int> &arr)
     {
         std::vector<int> tmp_arr = arr;
-        auto first = tmp_arr.begin();
-        auto last = tmp_arr.begin() + arr.size();
 
-        std::sort(first, last);
-        std::vector<int> res;
-        for (int i = 0; i < arr.size(); i++)
+        // Sort the copied array to get the ranking order
+        std::sort(tmp_arr.begin(), tmp_arr.end());
+
+        // Map to store the rank of each element
+        std::unordered_map<int, int> rank_map;
+        int rank = 1;
+
+        // Assign rank to each unique element
+        for (int num : tmp_arr)
         {
-            auto it = std::find(tmp_arr.begin(), tmp_arr.end(), arr[i]);
-            if (it != tmp_arr.end())
+            if (rank_map.find(num) == rank_map.end())
             {
-                int index = std::distance(tmp_arr.begin(), it);
-                res.push_back(index+1);
+                rank_map[num] = rank;
+                rank++;
             }
+        }
+
+        // Replace each element in the original array with its rank
+        std::vector<int> res;
+        for (int num : arr)
+        {
+            res.push_back(rank_map[num]);
         }
 
         return res;
     }
 };
+
 int main()
 {
     RankTransformArray_1331 r1;
-    std::vector<int> arr = {37,12,28,9,100,56,80,5,12};
+    std::vector<int> arr = {37, 12, 28, 9, 100, 56, 80, 5, 12};
     std::vector<int> res = r1.arrayRankTransform(arr);
+
     for (int i = 0; i < res.size(); i++)
     {
         std::cout << res[i] << " ";
     }
+    return 0;
 }
+static const int hansil = []()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return 0;
+}();
