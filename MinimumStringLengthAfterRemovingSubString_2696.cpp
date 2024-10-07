cd8cda95855dee641ff8cdcb6b5d9f6d0ee7fe65
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-10-07 16:56:20 +0530

    Old code added

--- MinimumStringLengthAfterRemovingSubString_2696.cpp
+++ MinimumStringLengthAfterRemovingSubString_2696.cpp
@@ -33,3 +33,85 @@ int main()
     std::cout << m1.minLength("DCDCBCBDACBBABDABABCCCBABCCCCCCCBDDBCDACDADABADDCDBBC") << std::endl;
     return 0;
 }
+
+/*
+#include <iostream>
+#include <string>
+class MinimumStringLengthAfterRemovingSubString_2696
+{
+private:
+    std::string removeAB(std::string s)
+    {
+        while (s.find("AB") != std::string::npos || s.find("CD") != std::string::npos)
+        {
+            std::string str = "";
+            if (int pos = s.find("AB") != std::string::npos)
+            {
+                if (pos == 1)
+                {
+                    str += s.substr(2, s.length());
+                    s = str;
+                }
+                else
+                {
+                    str += s.substr(0, pos);
+                    str += s.substr(pos + 1, s.length());
+                    s = str;
+                    std::cout << s << "\n";
+                }
+            }
+            else if(int pos = s.find("CD") != std::string::npos)
+            {
+                if (pos == 1)
+                {
+                    str += s.substr(2, s.length());
+                    s = str;
+                }
+                else
+                {
+                    str += s.substr(0, pos);
+                    str += s.substr(pos + 1, s.length());
+                    s = str;
+                    std::cout << s << "\n";
+                }
+            }
+        }
+        return s;
+    }
+    std::string removeCD(std::string s)
+    {
+        while (s.find("CD") != std::string::npos)
+        {
+            int pos = s.find("CD");
+            std::string str = "";
+            if (pos == 0)
+            {
+                str += s.substr(2, s.length());
+                s = str;
+            }
+            else
+            {
+                str += s.substr(0, pos);
+                str += s.substr(pos + 1, s.length());
+                s = str;
+                std::cout << s << "\n";
+            }
+        }
+    }
+
+public:
+    int minLength(std::string s)
+    {
+        s = removeAB(s);
+        return s.length();
+    }
+};
+int main()
+{
+    MinimumStringLengthAfterRemovingSubString_2696 m1;
+    std::cout << m1.minLength("DCDCBCBDACBBABDABABCCCBABCCCCCCCBDDBCDACDADABADDCDBBC");
+    return 0;
+}
+
+
+*/
