5625f2512ea89f437e3e8a820bb48dcf49feb165
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-10-08 18:53:58 +0530

    MinimumNumberSwapStringBalanced_1963.cpp template is ready

---
+++ MinimumNumberSwapStringBalanced_1963.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+class MinimumNumberSwapStringBalanced_1963
+{
+private:
+    /* data */
+public:
+    int minSwaps(std::string s)
+    {
+    }
+};
+int main()
+{
+    return 0;
+}
