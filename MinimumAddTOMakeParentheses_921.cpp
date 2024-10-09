36d2f95d916acae4b79ec5301eb207f776e2d0f1
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-10-09 13:10:37 +0530

    921 done

--- MinimumAddTOMakeParentheses_921.cpp
+++ MinimumAddTOMakeParentheses_921.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 class MinimumAddTOMakeParentheses_921
 {
 private:
@@ -6,9 +7,36 @@ private:
 public:
     int minAddToMakeValid(std::string s)
     {
+        int leftbraces = 0;
+        int rightbraces = 0;
+        int unbalanced = 0;
+        for (char c : s)
+        {
+            if (c == '(')
+            {
+                leftbraces++;
+            }
+            else
+            {
+               if (leftbraces > 0)
+                    leftbraces--;  // Match ']' with a previous unmatched '['
+                else
+                    unbalanced++;  // Increment unbalanced count if there is no matching '['
+            }
+        }
+        return leftbraces + unbalanced;
     }
 };
 int main()
 {
+    MinimumAddTOMakeParentheses_921 M1;
+    std::cout << M1.minAddToMakeValid("()))((");
     return 0;
 }
+static const int hansil = []()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return 0;
+}();
