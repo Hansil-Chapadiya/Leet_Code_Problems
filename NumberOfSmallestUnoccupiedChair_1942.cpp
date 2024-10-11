04159d44bd7bd2c408a0ce5a8553276868398eab
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-10-11 19:48:17 +0530

    NumberOfSmallestUnoccupiedChair_1942.cpp template is ready

---
+++ NumberOfSmallestUnoccupiedChair_1942.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+class NumberOfSmallestUnoccupiedChair_1942
+{
+private:
+    /* data */
+public:
+    int smallestChair(std::vector<std::vector<int>> &times, int targetFriend)
+    {
+
+    }
+};
+int main()
+{
+    return 0;
+}
