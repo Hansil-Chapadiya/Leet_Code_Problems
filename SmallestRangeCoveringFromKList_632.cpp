05a4809b06ddb6c71ae548579921fc53ed9be17c
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-10-13 18:39:58 +0530

    632 template is ready

---
+++ SmallestRangeCoveringFromKList_632.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+class SmallestRangeCoveringFromKList_632
+{
+private:
+public:
+    std::vector<int> smallestRange(std::vector<std::vector<int>> &nums)
+    {
+    }
+};
+int main()
+{
+}
