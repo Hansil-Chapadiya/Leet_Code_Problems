faf2983a00ac2d8dcb82e90a0a96d8bc55bd2832
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-10-14 11:04:28 +0530

    2530 template is ready

---
+++ MaximumScoreAfterApplyingKOperation_2530.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+class MaximumScoreAfterApplyingKOperation_2530
+{
+private:
+    /* data */
+public:
+    long long maxKelements(std::vector<int>& nums, int k) {
+
+    }
+};
+int main()
+{
+    return 0;
+}
+
