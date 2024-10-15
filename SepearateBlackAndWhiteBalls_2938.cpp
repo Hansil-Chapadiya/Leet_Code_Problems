4068b2dcb3c733b0c96907cc72179f0877536a3b
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-10-15 21:02:35 +0530

    SepearateBlackAndWhiteBalls_2938

---
+++ SepearateBlackAndWhiteBalls_2938.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+class SepearateBlackAndWhiteBalls_2938
+{
+private:
+    /* data */
+public:
+     long long minimumSteps(std::string s) {
+
+    }
+};
+int main()
+{
+    SepearateBlackAndWhiteBalls_2938 s1;
+    s1.minimumSteps("101");
+    return 0;
+}
