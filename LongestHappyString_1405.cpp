0895c271beec287f45c33f467614f2bcb9566043
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-10-16 11:53:28 +0530

    1405 template is ready

---
+++ LongestHappyString_1405.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <queue>
+#include <string>
+class LongestHappyString_1405
+{
+private:
+    /* data */
+public:
+    std::string longestDiverseString(int a, int b, int c)
+    {
+    }
+};
+int main()
+{
+    LongestHappyString_1405 L1;
+    return 0;
+}
