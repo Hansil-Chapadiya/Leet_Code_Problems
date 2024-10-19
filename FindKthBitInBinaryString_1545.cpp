e0bb99000ee2c6cdd6783dfb527ed10e19c14adb
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-10-19 10:33:00 +0530

    1545 template is ready

---
+++ FindKthBitInBinaryString_1545.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+class FindKthBitInBinaryString_1545
+{
+private:
+    std::string s1 = "0";
+public:
+    char findKthBit(int n, int k)
+    {
+        if (n == 1)
+        {
+            return n;
+        }
+        else
+        {
+
+        }
+
+    }
+};
+int main()
+{
+    FindKthBitInBinaryString_1545 k1;
+    k1.findKthBit(3,1);
+    return 0;
+}
