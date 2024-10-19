d3e500b1151239f73905a25d642f0e32f5cd8ae2
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-10-19 11:35:29 +0530

    1545 ready

--- FindKthBitInBinaryString_1545.cpp
+++ FindKthBitInBinaryString_1545.cpp
@@ -1,26 +1,44 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
 class FindKthBitInBinaryString_1545
 {
 private:
     std::string s1 = "0";
+    std::string invert(std::string str)
+    {
+        std::string str_ = "";
+        for (int i = 0; i < str.length(); i++)
+        {
+            if (str[i] == '0')
+            {
+                str_ += '1';
+            }
+            else
+            {
+                str_ += '0';
+            }
+        }
+        return str_;
+    }
+
 public:
     char findKthBit(int n, int k)
     {
-        if (n == 1)
+        for (int i = 1; i < n; i++)
         {
-            return n;
+            std::string temp = invert(s1);
+            std::reverse(temp.begin(), temp.end());
+            s1 = s1  + "1" + temp;
+            // std::cout<<s1<<std::endl;
         }
-        else
-        {
-
-        }
-
+        return s1[k-1];
     }
 };
 int main()
 {
     FindKthBitInBinaryString_1545 k1;
-    k1.findKthBit(3,1);
+    std::cout << k1.findKthBit(4, 11);
     return 0;
-}
+} // 011
+// 011
