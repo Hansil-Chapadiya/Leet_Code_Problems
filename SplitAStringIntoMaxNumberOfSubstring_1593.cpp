f93f652c1098cae957186e6609f70378513fea37
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-10-21 17:52:44 +0530

    template is ready

---
+++ SplitAStringIntoMaxNumberOfSubstring_1593.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+class SplitAStringIntoMaxNumberOfSubstring_1593
+{
+private:
+public:
+    int maxUniqueSplit(std::string s)
+    {
+    }
+};
+int main()
+{
+    SplitAStringIntoMaxNumberOfSubstring_1593 s1;
+    s1.maxUniqueSplit("ababccc");
+    return 0;
+}
+static const int hansil = []()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return 0;
+}();
