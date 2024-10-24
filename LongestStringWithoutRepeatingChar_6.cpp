c3ff07780ffa51a34485a35b137f3dcd41e69014
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-10-24 17:20:10 +0530

    Template is ready

---
+++ LongestStringWithoutRepeatingChar_6.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<unordered_map>
+#include<string>
+class LongestStringWithoutRepeatingChar_6
+{
+private:
+    /* data */
+public:
+    int lengthOfLongestSubstring(std::string s) {
+
+    }
+};
+int main()
+{
+    LongestStringWithoutRepeatingChar_6 l1;
+    std::cout<<l1.lengthOfLongestSubstring("abcabcbb");
+    return 0;
+}
+static const int hansil = []()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return 0;
+}();
