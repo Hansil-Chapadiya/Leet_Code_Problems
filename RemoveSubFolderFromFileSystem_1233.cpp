fb0e509bbd2dbafb347ab68bbd93aca4727a0fd6
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-10-25 09:05:32 +0530

    template is ready

---
+++ RemoveSubFolderFromFileSystem_1233.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+#include <iostream>
+#include <string>
+class RemoveSubFolderFromFileSystem_1233
+{
+private:
+    /* data */
+public:
+    std::vector<std::string> removeSubfolders(std::vector<std::string> &folder)
+    {
+    }
+};
+int main()
+{
+    RemoveSubFolderFromFileSystem_1233 r1;
+    std::vector<std::string> folder = {"/a", "/a/b", "/c/d", "/c/d/e", "/c/f"};
+    return 0;
+}
