b0587f99a880491ae2f5a8f78d95d84306c2d7ba
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-10-27 14:18:59 +0530

    1277 optimized code added

--- CountSubMatricWithAllOnes_1277.cpp
+++ CountSubMatricWithAllOnes_1277.cpp
@@ -35,3 +35,32 @@ int main()
     std::cout<<c1.countSquare(matrix);
     return 0;
 }
+
+/*
+
+class Solution {
+public:
+    int countSquares(std::vector<std::vector<int>>& matrix) {
+        int r = matrix.size();
+        int c = matrix[0].size();
+        std::vector<int> dp(c + 1, 0);
+        int answer = 0, prev = 0;
+
+        for (int i = 0; i < r; ++i) {
+            for (int j = 0; j < c; ++j) {
+                int temp = dp[j + 1];
+                if (matrix[i][j] == 1) {
+                    dp[j + 1] = std::min({dp[j], dp[j + 1], prev}) + 1;
+                    answer += dp[j + 1];
+                } else {
+                    dp[j + 1] = 0;
+                }
+                prev = temp;
+            }
+        }
+
+        return answer;
+    }
+};
+
+ */
