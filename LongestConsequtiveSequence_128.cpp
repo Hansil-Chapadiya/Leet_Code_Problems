2605f1d151cfac63c14aa43c907ef9e98a2c9215
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-10-28 16:06:51 +0530

    Template is ready

---
+++ LongestConsequtiveSequence_128.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+class LongestConsequtiveSequence_128
+{
+private:
+    /* data */
+public:
+    int longestConsecutive(std::vector<int> &nums)
+    {
+    }
+};
+int main()
+{
+    LongestConsequtiveSequence_128 l1;
+    std::vector<int> nums = {100, 4, 200, 1, 3, 2};
+    l1.longestConsecutive(nums);
+    return 0;
+}
