adf331e73a7ed52bff37670db96fe80ba76449a7
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-10-30 18:40:16 +0530

    1671 template is ready

---
+++ MakeMountainArray_1671.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+class MakeMountainArray_1671
+{
+private:
+    /* data */
+public:
+    int minimumMountainRemovals(std::vector<int> &nums)
+    {
+    }
+};
+int main()
+{
+    MakeMountainArray_1671 m1;
+    std::vector<int> nums = {2, 1, 1, 5, 6, 2, 3, 1};
+    m1.minimumMountainRemovals(nums);
+    return 0;
+}
