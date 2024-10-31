f74d2d176cb9b154aeeb6688934613cac62ad266
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-10-31 18:28:04 +0530

    2463 template is ready

---
+++ MinimumTotalDistanceTraveled_2463.cpp
@@ -0,0 +1,18 @@
+#include <queue>
+#include <vector>
+
+class MinimumTotalDistanceTraveled_2463
+{
+private:
+    /* data */
+public:
+    long long minimumTotalDistance(std::vector<int> &robot, std::vector<std::vector<int>> &factory)
+    {
+    }
+};
+int main()
+{
+    MinimumTotalDistanceTraveled_2463 m1;
+    std::vector<int> robot = {0, 4, 6};
+    std::vector<std::vector<int>> factory = {{2, 2}, {6, 2}};
+}
