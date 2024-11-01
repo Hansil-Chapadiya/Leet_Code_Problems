c0485284587ac09b648b28955fbd484b652b97a9
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-11-01 17:55:54 +0530

    1957 template is ready

---
+++ DeleteCharactersToMakeFancyString_1957.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <string>
+class DeleteCharactersToMakeFancyString_1957
+{
+private:
+    /* data */
+public:
+    std::string makeFancyString(std::string s)
+    {
+    }
+};
+int main()
+{
+    DeleteCharactersToMakeFancyString_1957 d1;
+    d1.makeFancyString("leeetcode");
+    return 0;
+}
