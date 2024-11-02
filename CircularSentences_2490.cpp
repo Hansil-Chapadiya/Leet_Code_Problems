595eb5d8b13f39d3a23972b1ef86e295b76c996e
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-11-02 18:26:14 +0530

    2490 template is ready

---
+++ CircularSentences_2490.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+class CircularSentences_2490
+{
+private:
+    /* data */
+public:
+    bool isCircularSentence(std::string sentence)
+    {
+    }
+};
+int main()
+{
+    CircularSentences_2490 c1;
+    std::cout<<c1.isCircularSentence("leetcode exercises sound delightful");
+    return 0;
+}
