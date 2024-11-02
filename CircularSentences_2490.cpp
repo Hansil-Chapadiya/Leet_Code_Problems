8b1554b699726ec23d679d1e58fbcf5a802a0619
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-11-02 18:49:01 +0530

    2490 is ready

--- CircularSentences_2490.cpp
+++ CircularSentences_2490.cpp
@@ -7,11 +7,38 @@ private:
 public:
     bool isCircularSentence(std::string sentence)
     {
+        bool flag = 1;
+        if (sentence[0] != sentence[sentence.length() - 1])
+        {
+            return false;
+        }
+
+        for (int i = 1; i < sentence.length(); i++)
+        {
+            if (sentence[i] == ' ')
+            {
+                if (sentence[i + 1] != '\0')
+                {
+                    if (sentence[i - 1] != sentence[i + 1])
+                    {
+                        flag = 0;
+                        break;
+                    }
+                }
+            }
+        }
+        return flag ? true : false;
+
     }
 };
 int main()
 {
     CircularSentences_2490 c1;
-    std::cout<<c1.isCircularSentence("leetcode exercises sound delightful");
+    // std::cout << c1.isCircularSentence("leetcode exercises sound delightful");
+    // std::cout << c1.isCircularSentence("a b c d e a");
+    // std::cout << c1.isCircularSentence("leetcode Exercises sound delightful");
+    std::cout << c1.isCircularSentence("IuTiUtGGsNydmacGduehPPGksKQyT TmOraUbCcQdnZUCpGCYtGp p pG GCcRvZDRawqGKOiBSLwjIDOjdhnHiisfddYoeHqxOqkUvOEyI");
+    // std::cout << c1.isCircularSentence("eetcode");
+    // std::cout << c1.isCircularSentence("Leetcode is cool");
     return 0;
 }
