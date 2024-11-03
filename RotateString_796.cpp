ebaa1feb280d349187f7be2f779216e175cca1bb
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-11-03 18:32:25 +0530

    RotateString 796 template is ready

---
+++ RotateString_796.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+class RotateString_796
+{
+private:
+    /* data */
+public:
+   bool rotateString(std::string s, std::string goal) {
+
+    }
+};
+int main()
+{
+    RotateString_796 r1;
+    r1.rotateString("abcde","cdeab");
+    return 0;
+}
