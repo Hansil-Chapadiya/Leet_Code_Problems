a98d80da3899e4b9ca4e930ee5da354991b92da8
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-11-04 18:23:08 +0530

    StringComparison_3163.cpp ready

--- StringComparisonIII_3163.cpp
+++ StringComparisonIII_3163.cpp
@@ -7,13 +7,38 @@ private:
 public:
     std::string compressedString(std::string word)
     {
+        if (word.length() == 1)
+        {
+            return "1" + word;
+        }
 
+        std::string comp;
+        comp = "";
+        int cnt = 1;
+        for (int i = 1; i <= word.length(); i++)
+        {
+            while (word[i] == word[i - 1] && cnt < 9)
+            {
+                cnt++;
+                if (i < word.length())
+                {
+                    i++;
+                }
+            }
+            comp += std::to_string(cnt);
+            comp += word[i - 1];
+            cnt = 1;
+        }
+        return comp;
     }
 };
 int main()
 {
     StringComparisonIII_3163 s1;
     std::string word = "abcde";
-    s1.compressedString(word);
+    // std::string word = "aaaaaaaaaaaaaabb";
+    // std::string word = "o";
+    // std::string word = "vvv";
+    std::cout << s1.compressedString(word);
     return 0;
 }
