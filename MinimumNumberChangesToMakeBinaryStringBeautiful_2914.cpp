d823e0e3d56c1c052240c2f3427fb282b938e93a
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-11-05 12:03:30 +0530

    MinimumNumberChangesToMakeBinaryStringBeautiful_2914.cpp template is ready

---
+++ MinimumNumberChangesToMakeBinaryStringBeautiful_2914.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+class MinimumNumberChangesToMakeBinaryStringBeautiful_2914
+{
+private:
+    /* data */
+public:
+    int minChanges(std::string s)
+    {
+    }
+};
+int main()
+{
+    MinimumNumberChangesToMakeBinaryStringBeautiful_2914 m1;
+    m1.minChanges("111101101110");
+    return 0;
+}
