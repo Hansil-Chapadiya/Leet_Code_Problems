540f48b7e050839f2603ef2ab0469a220656357c
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-11-06 17:57:46 +0530

    3011 template is ready

---
+++ FindArrayCanBeSorted_3011.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+
+class FindArrayCanBeSorted_3011
+{
+private:
+    /* data */
+public:
+    bool canSortArray(std::vector<int> &nums)
+    {
+        for (int i = 0; i < nums.size(); i++)
+        {
+            /* code */
+        }
+    }
+};
+int main()
+{
+    std::vector<int> nums = {8, 4, 2, 30, 15};
+    FindArrayCanBeSorted_3011 f1;
+    f1.canSortArray(nums);
+    return 0;
+}
