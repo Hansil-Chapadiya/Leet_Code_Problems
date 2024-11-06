5fa233fed2a5c73d0528b17bd45c37d2041d3eb8
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-11-06 19:06:11 +0530

    FindArrayCanBeSorted_3011.cpp ready

--- FindArrayCanBeSorted_3011.cpp
+++ FindArrayCanBeSorted_3011.cpp
@@ -4,20 +4,55 @@
 class FindArrayCanBeSorted_3011
 {
 private:
-    /* data */
+    // Function to count the number of set bits in a number
+    int countSetBits(int num)
+    {
+        int count = 0;
+        while (num)
+        {
+            count += (num & 1);
+            num >>= 1;
+        }
+        return count;
+    }
+
 public:
+    // Function to check if we can sort the array by set bit count
     bool canSortArray(std::vector<int> &nums)
     {
-        for (int i = 0; i < nums.size(); i++)
+        int n = nums.size();
+
+        // Perform adjacent swaps to achieve sorted order by set bit count and value
+        for (int i = 0; i < n - 1; ++i)
         {
-            /* code */
+            for (int j = 0; j < n - 1; ++j)
+            {
+                int count1 = countSetBits(nums[j]);
+                int count2 = countSetBits(nums[j + 1]);
+
+                // Swap if adjacent elements have the same set bit count and are out of order
+                if (count1 == count2 && nums[j] > nums[j + 1])
+                {
+                    std::swap(nums[j], nums[j + 1]);
+                }
+            }
         }
+
+        // Check if the array is now sorted in ascending order
+        for (int i = 1; i < n; i++)
+        {
+            if (nums[i] < nums[i - 1])
+            {
+                return false;
+            }
+        }
+        return true;
     }
 };
 int main()
 {
     std::vector<int> nums = {8, 4, 2, 30, 15};
     FindArrayCanBeSorted_3011 f1;
-    f1.canSortArray(nums);
+    std::cout << f1.canSortArray(nums);
     return 0;
 }
