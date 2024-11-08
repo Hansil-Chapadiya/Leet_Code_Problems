89b96423a4d511c650490135e574ad28df844c79
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-11-08 18:39:22 +0530

    1829 template is ready

---
+++ MaximumXorForEachQuery_1829.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+class MaximumXorForEachQuery
+{
+private:
+    /* data */
+public:
+    std::vector<int> getMaximumXor(std::vector<int> &nums, int maximumBit)
+    {
+    }
+};
+int main()
+{
+    MaximumXorForEachQuery m1;
+    std::vector<int> nums = {0,1,1,3};
+    m1.getMaximumXor(nums, 2);
+    return 0;
+}
