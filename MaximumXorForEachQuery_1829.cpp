51dee3d40568d3d0fcd4c0d28a80b8240992c976
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-11-08 19:05:45 +0530

    another appreoach

--- MaximumXorForEachQuery_1829.cpp
+++ MaximumXorForEachQuery_1829.cpp
@@ -7,19 +7,34 @@ private:
 public:
     std::vector<int> getMaximumXor(std::vector<int> &nums, int maximumBit)
     {
-        int xor_product = 0;
-        for (int i = 0; i < nums.size(); i++)
+        // int xor_product = 0;
+        // for (int i = 0; i < nums.size(); i++)
+        // {
+        //     xor_product ^= nums[i];
+        // }
+
+        // std::vector<int> ans(nums.size(), 0);
+        // int mask = (1 << maximumBit) - 1;
+        // for (int i = 0; i < nums.size(); i++)
+        // {
+        //     int k = xor_product ^ mask;
+        //     ans[i] = k;
+        //     xor_product ^= nums[nums.size() - 1 - i];
+        // }
+        // return ans;
+
+        std::vector<int> xor_product(nums.size());
+        for (int i = 1; i < nums.size(); i++)
         {
-            xor_product ^= nums[i];
+            xor_product[i] = xor_product[i -1] ^ nums[i];
         }
 
         std::vector<int> ans(nums.size(), 0);
         int mask = (1 << maximumBit) - 1;
         for (int i = 0; i < nums.size(); i++)
         {
-            int k = xor_product ^ mask;
-            ans[i] = k;
-            xor_product ^= nums[nums.size() - 1 - i];
+            int product = xor_product[nums.size() - 1 - i];
+            ans[i] = product ^ mask;
         }
         return ans;
     }
