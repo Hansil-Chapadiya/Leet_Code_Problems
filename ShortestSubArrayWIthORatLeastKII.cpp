3eac4c596c41809ff81cc9df3284acd3d610ab20
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-11-10 18:21:28 +0530

    templete is ready

---
+++ ShortestSubArrayWIthORatLeastKII.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+class ShortestSubArrayWIthORatLeastKII
+{
+private:
+    /* data */
+public:
+    int minimumSubarrayLength(std::vector<int>& nums, int k) {
+
+    }
+};
+int main()
+{
+    ShortestSubArrayWIthORatLeastKII s1;
+    std::vector<int> nums = {1,2,3};
+    std::cout<<s1.minimumSubarrayLength(nums,2);
+    return 0;
+}
