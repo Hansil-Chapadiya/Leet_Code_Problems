f6aacaabd14520d030e0f49297447ca62b25016c
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-11-11 08:40:29 +0530

    template is ready

---
+++ PrimeSubtractionOparation_2601.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+class PrimeSubtractionOparation_2601
+{
+private:
+    /* data */
+public:
+    bool primeSubOperation(std::vector<int> &nums)
+    {
+    }
+};
+int main()
+{
+    PrimeSubtractionOparation_2601 p1;
+    std::vector<int> nums = {4, 9, 6, 10};
+    p1.primeSubOperation(nums);
+    return 0;
+}
