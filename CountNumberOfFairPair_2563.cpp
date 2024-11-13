7b9b6316c2d9ba5245e2cc323a3749799761ca60
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-11-13 09:49:05 +0530

    2563 template is ready

---
+++ CountNumberOfFairPair_2563.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+class CountNumberOfFairPair_2563
+{
+private:
+    /* data */
+public:
+    long long countFairPairs(std::vector<int> &nums, int lower, int upper)
+    {
+
+    }
+};
+int main()
+{
+    CountNumberOfFairPair_2563 c1;
+    std::vector<int> nums = {0, 1, 7, 4, 4, 5};
+    c1.countFairPairs(nums, 3, 6);
+    return 0;
+}
