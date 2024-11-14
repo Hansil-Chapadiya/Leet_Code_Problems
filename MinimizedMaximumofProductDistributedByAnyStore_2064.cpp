66310a0ff08f63ad8f162c9d577e998fed3af997
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-11-14 10:02:55 +0530

    2064 template is readyu

---
+++ MinimizedMaximumofProductDistributedByAnyStore_2064.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+class MinimizedMaximumofProductDistributedByAnyStore_2064
+{
+private:
+    /* data */
+public:
+    int minimizedMaximum(int n, std::vector<int> &quantities)
+    {
+    }
+};
+int main()
+{
+    std::vector<int> quantities = {11,6};
+    int stores = 6;
+    MinimizedMaximumofProductDistributedByAnyStore_2064 m1;
+    m1.minimizedMaximum(stores, quantities);
+    return 0;
+}
