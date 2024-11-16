49c0d6dfe8766ed3c645d5fb13155ea4e9cfc3a4
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-11-16 18:02:03 +0530

    3254 template is ready

---
+++ FindThePowerOfKSizeSubArrayI_3254.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+class FindThePowerOfKSizeSubArrayI_3254
+{
+private:
+    /* data */
+public:
+    std::vector<int> resultsArray(std::vector<int> &nums, int k)
+    {
+
+    }
+};
+int main()
+{
+    FindThePowerOfKSizeSubArrayI_3254 f1;
+    std::vector<int> nums = {1, 2, 3, 4, 3, 2, 5};
+    f1.resultsArray(nums, 3);
+    return 0;
+}
