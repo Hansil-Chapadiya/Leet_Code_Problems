b6c55ea83400b0484cac91e4bc8cc4e5373e497d
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-11-17 13:40:10 +0530

    862 template is ready

---
+++ ShortestSubArrayAtLeastK_862.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <deque>
+class ShortestSubArrayAtLeastK_862
+{
+private:
+    /* data */
+public:
+    int shortestSubarray(std::vector<int> &nums, int k)
+    {
+    }
+};
+int main()
+{
+    std::vector<int> nums = {2, -1, 2};
+    ShortestSubArrayAtLeastK_862 s1;
+    s1.shortestSubarray(nums, 3);
+    return 0;
+}
