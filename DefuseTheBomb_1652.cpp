6f1e1804467ffd0cd3cc0ec19adb1b6c60166590
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-11-18 19:28:20 +0530

    1652 template is ready

---
+++ DefuseTheBomb_1652.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+class DefuseTheBomb_1652
+{
+private:
+    /* data */
+public:
+    std::vector<int> decrypt(std::vector<int> &code, int k)
+    {
+    }
+};
+int main()
+{
+    DefuseTheBomb_1652 d1;
+    std::vector<int> code = {5,7,1,4};
+    d1.decrypt(code, 3);
+    return 0;
+}
