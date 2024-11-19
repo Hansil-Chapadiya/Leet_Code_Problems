042cfa3cf6a746834b0cb1c4f7d2c96690612eab
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-11-19 08:36:31 +0530

    MaximumSumOfDistnictSubArrayWithLengthK_2461.cpp template is readt

---
+++ MaximumSumOfDistnictSubArrayWithLengthK_2461.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+class MaximumSumOfDistnictSubArrayWithLengthK_2461
+{
+private:
+    /* data */
+public:
+    long long maximumSubarraySum(std::vector<int> &nums, int k)
+    {
+    }
+};
+int main()
+{
+    MaximumSumOfDistnictSubArrayWithLengthK_2461 m1;
+    std::vector<int> nums = {1, 5, 4, 2, 9, 9, 9};
+    m1.maximumSubarraySum(nums, 3);
+    return 0;
+}
+static const int hansil = []()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return 0;
+}();
