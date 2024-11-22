6bedd8945ff146b0d5e26b6dad877873f8ac0746
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-11-22 18:03:29 +0530

    template is ready

---
+++ FlipColumnsForMaximumNumberOfEqualRow_1072.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+class FlipColumnsForMaximumNumberOfEqualRow_1072
+{
+private:
+    /* data */
+public:
+    int maxEqualRowsAfterFlips(std::vector<std::vector<int>> &matrix)
+    {
+
+    }
+};
+int main()
+{
+    FlipColumnsForMaximumNumberOfEqualRow_1072 f1;
+    std::vector<std::vector<int>> matrix = {{0, 1}, {1, 1}};
+    return 0;
+}
