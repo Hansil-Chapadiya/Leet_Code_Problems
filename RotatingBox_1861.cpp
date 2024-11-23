9d2c6aeafcf41533b8c287f0e8cdaa994ca43851
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-11-23 12:42:33 +0530

    Rotating Box _1861.cpp

---
+++ RotatingBox_1861.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+class RotatingBox_1861
+{
+private:
+    /* data */
+public:
+    std::vector<std::vector<char>> rotateTheBox(std::vector<std::vector<char>> &box)
+    {
+    }
+};
+int main()
+{
+    RotatingBox_1861 r1;
+    std::vector<std::vector<char>> box = {{'#', '.', '#'}};
+    r1.rotateTheBox(box);
+    return 0;
+}
