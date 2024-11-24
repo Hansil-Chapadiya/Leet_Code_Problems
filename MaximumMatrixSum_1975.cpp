e8eb71dc4c20af0b199d65d2294ab4c2ea3823f4
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-11-24 11:01:48 +0530

    Template is 1975 ready

---
+++ MaximumMatrixSum_1975.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+class MaximumMatrixSum_1975
+{
+private:
+    /* data */
+public:
+    long long maxMatrixSum(std::vector<std::vector<int>> &matrix)
+    {
+    }
+};
+int main()
+{
+    std::vector<std::vector<int>> matrix = {{1, -1}, {-1, 1}};
+    MaximumMatrixSum_1975 m1;
+    m1.maxMatrixSum(matrix);
+    return 0;
+}
