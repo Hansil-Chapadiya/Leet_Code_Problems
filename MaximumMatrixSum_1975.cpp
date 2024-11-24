467529b43d71756e5c493d02757e8f11eba8e9f2
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-11-24 11:42:56 +0530

    MaximumMatrixSum_1975

--- MaximumMatrixSum_1975.cpp
+++ MaximumMatrixSum_1975.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <cstdlib>
+#include <climits>
 class MaximumMatrixSum_1975
 {
 private:
@@ -7,12 +9,33 @@ private:
 public:
     long long maxMatrixSum(std::vector<std::vector<int>> &matrix)
     {
+        long long sum = 0;
+        int count_negative = 0;
+        int min_element = INT_MAX;
+        for (int i = 0; i < matrix.size(); i++)
+        {
+            for (int j = 0; j < matrix[0].size(); j++)
+            {
+                sum += std::abs(matrix[i][j]);
+                min_element = std::min(min_element, std::abs(matrix[i][j]));
+                if (matrix[i][j] < 0)
+                {
+                    count_negative++;
+                }
+            }
+        }
+
+        if (count_negative % 2 != 0)
+        {
+            return sum - (2 * min_element);
+        }
+        return sum;
     }
 };
 int main()
 {
     std::vector<std::vector<int>> matrix = {{1, -1}, {-1, 1}};
     MaximumMatrixSum_1975 m1;
-    m1.maxMatrixSum(matrix);
+    std::cout<<m1.maxMatrixSum(matrix);
     return 0;
 }
