c24f5959b1c9b8600ba4a44ade5def6b0a8d8457
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-11-27 14:39:10 +0530

    ShortestDistanceAfterAdditionRoadQueriesI_3243.cpp

---
+++ ShortestDistanceAfterAdditionRoadQueriesI_3243.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+class ShortestDistanceAfterAdditionRoadQueriesI_3243
+{
+private:
+    /* data */
+public:
+    std::vector<int> shortestDistanceAfterQueries(int n, std::vector<std::vector<int>> &queries)
+    {
+    }
+};
+int main()
+{
+    std::vector<std::vector<int>> queries = {{2, 4}, {0, 2}, {0, 4}};
+    ShortestDistanceAfterAdditionRoadQueriesI_3243 s1;
+    s1.shortestDistanceAfterQueries(4,queries);
+}
