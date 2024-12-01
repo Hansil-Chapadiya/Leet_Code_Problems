deb7132f779011e496f9da8fc646601693782cf3
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-12-01 14:54:26 +0530

    1346 template is ready

---
+++ CheckIfNanditsDoubleExists_1346.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+class CheckIfNanditsDoubleExists_1346
+{
+private:
+    /* data */
+public:
+    bool checkIfExist(std::vector<int> &arr)
+    {
+    }
+};
+int main()
+{
+    CheckIfNanditsDoubleExists_1346 c1;
+    std::vector<int> arr = {10,2,5,3};
+    c1.checkIfExist(arr);
+    return 0;
+}
