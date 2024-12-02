4f353c08d88e35adf134c0790112014c9d175851
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-12-02 21:26:35 +0530

    static function added

--- CountIfAWordOccursAsAPrefixOfWord_1455.cpp
+++ CountIfAWordOccursAsAPrefixOfWord_1455.cpp
@@ -48,3 +48,10 @@ int main()
     std::cout << c1.isPrefixOfWord(sentence, searchword);
     return 0;
 }
+static const int hansil = []()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return 0;
+}();
