579036f551492fc14cac37454c00c56612088083
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-12-03 15:48:57 +0530

    another approach is added

--- AddingSpacingToAString_2109.cpp
+++ AddingSpacingToAString_2109.cpp
@@ -13,13 +13,25 @@ public:
         int j = 0;
         for (int i = 0; i < spaces.size(); i++)
         {
-            s.insert(spaces[i]+j, " ");
+            s.insert(spaces[i] + j, " ");
             j++;
         }
         return s;
 
         /*
+        std::string result;
+            result.reserve(s.size() + spaces.size()); // Pre-allocate memory to avoid reallocations
 
+            int j = 0; // Index for spaces vector
+            for (int i = 0; i < s.size(); i++) {
+                if (j < spaces.size() && i == spaces[j]) {
+                    result += ' '; // Insert space
+                    j++;
+                }
+                result += s[i]; // Add current character
+            }
+
+            return result;
         */
     }
 };
@@ -28,5 +40,5 @@ int main()
     AddingSpacingToAString_2109 a1;
     std::vector<int> spaces = {5, 9};
     std::string s = "EnjoyYourCoffee";
-    std::cout<<a1.addSpaces(s, spaces);
+    std::cout << a1.addSpaces(s, spaces);
 }
