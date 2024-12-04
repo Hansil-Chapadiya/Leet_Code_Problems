5f2b25f398c73caceb82dabd5c29a627b87a5e80
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-12-04 14:53:32 +0530

    MakeStringASubsequenceUsingCyclicIncrements_2825.cpp template is ready

---
+++ MakeStringASubsequenceUsingCyclicIncrements_2825.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+class MakeStringASubsequenceUsingCyclicIncrements_2825
+{
+private:
+    /* data */
+public:
+    bool canMakeSubsequence(std::string str1, std::string str2)
+    {
+    }
+};
+int main()
+{
+    std::string str1 = "abc";
+    std::string str2 = "ad";
+    return 0;
+}
+static const int hansil = []()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return 0;
+}();
