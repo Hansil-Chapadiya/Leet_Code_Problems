42741e4d64da5bedce4dd4525b97408919cd4cf1
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-12-04 15:25:43 +0530

    MakeStringASubsequenceUsingCyclicIncrements_2825.cpp  done

--- MakeStringASubsequenceUsingCyclicIncrements_2825.cpp
+++ MakeStringASubsequenceUsingCyclicIncrements_2825.cpp
@@ -8,12 +8,44 @@ private:
 public:
     bool canMakeSubsequence(std::string str1, std::string str2)
     {
+        int i = 0;
+        int j = 0;
+        while (i < str1.length() && j < str2.length())
+        {
+            char c;
+            if (str1[i] == 'z')
+            {
+                c = 'a';
+            }
+            else
+            {
+                c = str1[i] + 1;
+            }
+            if (str1[i] == str2[j] || (c) == str2[j])
+            {
+                i++;
+                j++;
+            }
+            else
+            {
+                i++;
+            }
+        }
+        if (j == str2.length())
+        {
+            return true;
+        }
+        return false;
     }
 };
 int main()
 {
-    std::string str1 = "abc";
+    MakeStringASubsequenceUsingCyclicIncrements_2825 m1;
+    // std::string str1 = "abc";
+    // std::string str2 = "ad";
+    std::string str1 = "zc";
     std::string str2 = "ad";
+    std::cout << m1.canMakeSubsequence(str1, str2);
     return 0;
 }
 static const int hansil = []()
