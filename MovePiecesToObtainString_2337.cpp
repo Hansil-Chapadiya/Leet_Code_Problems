b40a0a0e514f7932705a84f0b266d5e0a9c3990f
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-12-05 17:10:35 +0530

    MovePiecesToObtainString_2337.cpp template is ready

---
+++ MovePiecesToObtainString_2337.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+class MovePiecesToObtainString_2337
+{
+private:
+    /* data */
+public:
+    bool canChange(std::string start, std::string target)
+    {
+    }
+};
+int main()
+{
+    MovePiecesToObtainString_2337 m1;
+    m1.canChange("_L__R__R_", "L______RR");
+}
+static const int hansil = []()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return 0;
+}();
