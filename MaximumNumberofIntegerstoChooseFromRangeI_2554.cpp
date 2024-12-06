b0029c5b07af2e68ccb26e299b3e80772b5aa27e
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-12-06 14:42:05 +0530

    MaximumNumberofIntegerstoChooseFromRangeI_2554.cpp template is ready

---
+++ MaximumNumberofIntegerstoChooseFromRangeI_2554.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+class MaximumNumberofIntegerstoChooseFromRangeI_2554
+{
+private:
+    /* data */
+public:
+    int maxCount(std::vector<int> &banned, int n, int maxSum)
+    {
+    }
+};
+int main()
+{
+    std::vector<int> banned = {1, 6, 5};
+    int n = 5;
+    int maxSum = 6;
+    return 0;
+}
