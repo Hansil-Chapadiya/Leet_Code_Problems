0772b269a7ce48450c3be0c00c5986d056d3eca4
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-12-09 22:56:34 +0530

    SpecialArrayII_3152.cpp tle approach

--- SpecialArrayII_3152.cpp
+++ SpecialArrayII_3152.cpp
@@ -7,13 +7,46 @@ private:
 public:
     std::vector<bool> isArraySpecial(std::vector<int> &nums, std::vector<std::vector<int>> &queries)
     {
+        std::vector<bool> special;
+        for (auto q : queries)
+        {
+            int flag = 1;
+            for (int i = q[0]; i < q[1]; i++)
+            {
+                // if ((i + 1) != (q[1]+1))
+                {
+                    if ((nums[i + 1] % 2 == 0 && nums[i] % 2 == 0) || (nums[i + 1] % 2 == 1 && nums[i] % 2 == 1))
+                    {
+                        // special.push_back(false);
+                        flag = 0;
+                        break;
+                    }
+                }
+            }
+            if (flag)
+            {
+                special.push_back(true);
+            }
+            else
+            {
+                special.push_back(false);
+            }
+        }
+        return special;
     }
 };
 int main()
 {
-    std::vector<int> nums = {3, 4, 1, 2, 6};
-    std::vector<std::vector<int>> queries = {{0, 4}};
+    // std::vector<int> nums = {3, 4, 1, 2, 6};
+    std::vector<int> nums = {4, 3, 1, 6};
+    // std::vector<std::vector<int>> queries = {{0, 4}};
+    std::vector<std::vector<int>> queries = {{0, 2}, {2, 3}};
     SpecialArrayII_3152 s1;
-    s1.isArraySpecial(nums, queries);
+    std::vector<bool> special = s1.isArraySpecial(nums, queries);
+    for (int i = 0; i < special.size(); i++)
+    {
+        std::cout << special[i] << " ";
+    }
+
     return 0;
 }
