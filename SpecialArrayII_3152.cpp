111291d30b2575ef04f538516746f18157817bb8
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-12-09 23:05:04 +0530

    SpecialArrayII_3152.cpp done

--- SpecialArrayII_3152.cpp
+++ SpecialArrayII_3152.cpp
@@ -1,52 +1,53 @@
 #include <iostream>
 #include <vector>
+
 class SpecialArrayII_3152
 {
-private:
-    /* data */
 public:
+    // Function to check if the subarrays are special
     std::vector<bool> isArraySpecial(std::vector<int> &nums, std::vector<std::vector<int>> &queries)
     {
-        std::vector<bool> special;
-        for (auto q : queries)
+        int n = nums.size();
+
+        // Step 1: Create parityChange array
+        std::vector<int> parityChange(n - 1, 0);
+        for (int i = 0; i < n - 1; i++)
         {
-            int flag = 1;
-            for (int i = q[0]; i < q[1]; i++)
-            {
-                // if ((i + 1) != (q[1]+1))
-                {
-                    if ((nums[i + 1] % 2 == 0 && nums[i] % 2 == 0) || (nums[i + 1] % 2 == 1 && nums[i] % 2 == 1))
-                    {
-                        // special.push_back(false);
-                        flag = 0;
-                        break;
-                    }
-                }
-            }
-            if (flag)
-            {
-                special.push_back(true);
-            }
-            else
-            {
-                special.push_back(false);
-            }
+            parityChange[i] = (nums[i] % 2 != nums[i + 1] % 2) ? 1 : 0;
         }
+
+        // Step 2: Create prefix sum array for parityChange
+        std::vector<int> parityPrefix(n, 0);
+        for (int i = 1; i < n; i++)
+        {
+            parityPrefix[i] = parityPrefix[i - 1] + parityChange[i - 1];
+        }
+
+        // Step 3: Answer the queries
+        std::vector<bool> special;
+        for (auto &q : queries)
+        {
+            int from = q[0], to = q[1];
+            // Calculate total number of parity changes in the subarray [from, to]
+            int totalChanges = parityPrefix[to] - (from > 0 ? parityPrefix[from - 1] : 0);
+
+            // If total changes is exactly (to - from), then it is "special"
+            special.push_back(totalChanges == (to - from));
+        }
+
         return special;
     }
 };
+
 int main()
 {
-    // std::vector<int> nums = {3, 4, 1, 2, 6};
     std::vector<int> nums = {4, 3, 1, 6};
-    // std::vector<std::vector<int>> queries = {{0, 4}};
     std::vector<std::vector<int>> queries = {{0, 2}, {2, 3}};
     SpecialArrayII_3152 s1;
     std::vector<bool> special = s1.isArraySpecial(nums, queries);
-    for (int i = 0; i < special.size(); i++)
+    for (bool val : special)
     {
-        std::cout << special[i] << " ";
+        std::cout << val << " ";
     }
-
     return 0;
 }
