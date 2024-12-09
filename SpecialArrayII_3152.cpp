9615184dab3bfcc53e20b1b711055a5fe75f7784
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-12-09 21:42:32 +0530

    3152_templte is ready

---
+++ SpecialArrayII_3152.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+class SpecialArrayII_3152
+{
+private:
+    /* data */
+public:
+    std::vector<bool> isArraySpecial(std::vector<int> &nums, std::vector<std::vector<int>> &queries)
+    {
+    }
+};
+int main()
+{
+    std::vector<int> nums = {3, 4, 1, 2, 6};
+    std::vector<std::vector<int>> queries = {{0, 4}};
+    SpecialArrayII_3152 s1;
+    s1.isArraySpecial(nums, queries);
+    return 0;
+}
