318e077973eae767990ebbca9c3cd3d7c9b6275e
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-12-10 08:25:20 +0530

    FindLongesSpecialSubstringThatOccursThriceI_2981.cpp template is ready

---
+++ FindLongesSpecialSubstringThatOccursThriceI_2981.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+class FindLongesSpecialSubstringThatOccursThriceI_2981
+{
+private:
+    /* data */
+public:
+    int maximumLength(std::string s) {
+
+    }
+};
+int main()
+{
+    FindLongesSpecialSubstringThatOccursThriceI_2981 f1;
+    f1.maximumLength("aaaa");
+    return 0;
+}
