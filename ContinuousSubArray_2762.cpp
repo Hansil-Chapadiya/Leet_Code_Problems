2df0c34eda494786508e5e9458d52e5f028eb6b9
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-12-14 14:37:33 +0530

    ContinuousSubArray_2762.cpp template is ready

---
+++ ContinuousSubArray_2762.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+class ContinuousSubArray_2762
+{
+private:
+    /* data */
+public:
+    long long continuousSubarrays(std::vector<int> &nums)
+    {
+    }
+};
+int main()
+{
+    std::vector<int> nums = {};
+    ContinuousSubArray_2762 c1;
+    c1.continuousSubarrays(nums);
+    return 0;
+}
+static const int hansil = []()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return 0;
+}();
