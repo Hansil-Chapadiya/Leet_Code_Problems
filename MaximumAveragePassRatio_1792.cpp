2483ca78b4558f572109a54617b5b1b5947f2e26
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-12-15 13:05:10 +0530

    MaximumAveragePassRatio_1792.cpp template is reay

---
+++ MaximumAveragePassRatio_1792.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+class MaximumAveragePassRatio_1792
+{
+private:
+    /* data */
+public:
+    double maxAverageRatio(std::vector<std::vector<int>> &classes, int extraStudents)
+    {
+    }
+};
+int main()
+{
+    std::vector<std::vector<int>> classes = {{1, 2}, {3, 5}, {2, 2}};
+    int extrastudents = 4;
+    return 0;
+}
