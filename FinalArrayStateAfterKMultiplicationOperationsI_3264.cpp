11b15592bd3faad52cabec5213a55c699112de9f
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-12-16 17:23:33 +0530

    3264 template is ready

---
+++ FinalArrayStateAfterKMultiplicationOperationsI_3264.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+class FinalArrayStateAfterKMultiplicationOperationsI_3264
+{
+private:
+    /* data */
+public:
+    std::vector<int> getFinalState(std::vector<int> &nums, int k, int multiplier)
+    {
+    }
+};
+
+int main()
+{
+    FinalArrayStateAfterKMultiplicationOperationsI_3264 f1;
+    return 0;
+}
+static const int hansil = []()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return 0;
+}();
