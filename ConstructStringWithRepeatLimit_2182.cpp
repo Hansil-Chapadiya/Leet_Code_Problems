4e8d24e02b29d9ed16e07e85c722e089f8ff41fb
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-12-17 16:19:47 +0530

    TLE approach

--- ConstructStringWithRepeatLimit_2182.cpp
+++ ConstructStringWithRepeatLimit_2182.cpp
@@ -4,31 +4,77 @@
 
 class ConstructStringWithRepeatLimit_2182
 {
-private:
-    /* data */
 public:
     std::string repeatLimitedString(std::string s, int repeatLimit)
     {
-        int cnt = 1;
         std::priority_queue<char> pq;
-        for (auto i : s)
+        for (char i : s)
         {
             pq.push(i);
         }
 
-        for (int i = 1; i < s.length(); i++)
+        int cnt = 1;
+        char last_top = pq.top();
+        pq.pop();
+        std::string repeatLimitedString;
+        repeatLimitedString += last_top;
+
+        while (!pq.empty())
         {
-            if (s[i] == s[i - 1])
+            if (last_top == pq.top())
             {
                 cnt++;
             }
+            else
+            {
+                cnt = 1;
+            }
+
+            if (cnt <= repeatLimit)
+            {
+                repeatLimitedString += pq.top();
+                last_top = pq.top();
+                pq.pop();
+            }
+            else
+            {
+                char repeat_char = pq.top();
+                int repeat = 0;
+
+                // Make sure we do not access pq.top() if pq is empty
+                while (!pq.empty() && pq.top() == repeat_char)
+                {
+                    repeat++;
+                    pq.pop();
+                }
+
+                if (pq.empty())
+                {
+                    return repeatLimitedString;
+                }
+                else
+                {
+                    repeatLimitedString += pq.top();
+                    last_top = pq.top();
+                    pq.pop();
+
+                    while (repeat > 0)
+                    {
+                        pq.push(repeat_char);
+                        repeat--; // Decrement repeat to avoid infinite loop
+                    }
+                }
+            }
         }
+
+        return repeatLimitedString;
     }
 };
+
 int main()
 {
     std::string s = "cczazcc";
     ConstructStringWithRepeatLimit_2182 c1;
-    c1.repeatLimitedString(s, 3);
+    std::cout << c1.repeatLimitedString(s, 3);
     return 0;
 }
