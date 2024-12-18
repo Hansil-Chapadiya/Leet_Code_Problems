cf42bf96924d5438f729640e5b9bb44bb959c22b
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-12-18 09:12:49 +0530

    FinalPricesWithSpecialDiscountShop_1475.cpp template is ready

---
+++ FinalPricesWithSpecialDiscountShop_1475.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+class FinalPricesWithSpecialDiscountShop_1475
+{
+private:
+    /* data */
+public:
+    std::vector<int> finalPrices(std::vector<int> &prices)
+    {
+    }
+};
+int main()
+{
+    FinalPricesWithSpecialDiscountShop_1475 f1;
+    std::vector<int> prices = {};
+    std::vector<int> ans = f1.finalPrices(prices);
+    return 0;
+}
+static const int hansil = []()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return 0;
+}();
