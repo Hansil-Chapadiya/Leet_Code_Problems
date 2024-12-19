20ccb438238f6985ff7cb4d9b5e6098363dc9c1f
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-12-19 17:38:16 +0530

    MaxChunkToMakeSorted_769.cpp template is ready

---
+++ MaxChunkToMakeSorted_769.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+class MaxChunkToMakeSorted_769
+{
+private:
+    /* data */
+public:
+    int maxChunksToSorted(std::vector<int> &arr)
+    {
+    }
+};
+int main()
+{
+    MaxChunkToMakeSorted_769 m1;
+    std::vector<int> arr = {4, 3, 2, 1, 0};
+    m1.maxChunksToSorted(arr);
+    return 0;
+}
+static const int hansil = []()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return 0;
+}();
