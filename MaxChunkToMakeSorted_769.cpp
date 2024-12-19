9e0c1f7bb9e9ed4cbd0ac27fb9bd2ca1e1379864
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-12-19 17:57:38 +0530

    MaxChunkToMakeSorted_769.cpp done

--- MaxChunkToMakeSorted_769.cpp
+++ MaxChunkToMakeSorted_769.cpp
@@ -7,13 +7,27 @@ private:
 public:
     int maxChunksToSorted(std::vector<int> &arr)
     {
+        int sum_series = 0;
+        int max_chunk = 0;
+        int ith_sum = 0;
+        for (int i = 0; i < arr.size(); i++)
+        {
+            sum_series += arr[i];
+            ith_sum = i * (i + 1) / 2;
+            if (ith_sum == sum_series)
+            {
+                max_chunk++;
+            }
+        }
+        return max_chunk;
     }
 };
 int main()
 {
     MaxChunkToMakeSorted_769 m1;
-    std::vector<int> arr = {4, 3, 2, 1, 0};
-    m1.maxChunksToSorted(arr);
+    // std::vector<int> arr = {4, 3, 2, 1, 0};
+    std::vector<int> arr = {1,0,2,3,4};
+    std::cout << m1.maxChunksToSorted(arr);
     return 0;
 }
 static const int hansil = []()
