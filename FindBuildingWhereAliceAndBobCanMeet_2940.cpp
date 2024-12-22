cad90b61c535a2aea9766d1d4c247e44c3b94883
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-12-22 11:34:17 +0530

    FindBuildingWhereAliceAndBobCanMeet_2940.cpp template is ready

---
+++ FindBuildingWhereAliceAndBobCanMeet_2940.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+class FindBuildingWhereAliceAndBobCanMeet_2940
+{
+private:
+    /* data */
+public:
+    std::vector<int> leftmostBuildingQueries(std::vector<int> &heights, std::vector<std::vector<int>> &queries)
+    {
+    }
+};
+int main()
+{
+    FindBuildingWhereAliceAndBobCanMeet_2940 f1;
+    std::vector<int> height = {6, 4, 8, 5, 2, 7};
+    std::vector<std::vector<int>> queries = {{0, 1}, {0, 3}, {2, 4}, {3, 4}, {2, 2}};
+    return 0;
+}
