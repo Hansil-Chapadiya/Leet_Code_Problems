6b330db795d0055de77fd1237d4560ca921f3bcc
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-12-24 19:58:30 +0530

    FindMinimumDiameterAfterMergingTwoTrees_3203.cpp template is ready

---
+++ FindMinimumDiameterAfterMergingTwoTrees_3203.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+class FindMinimumDiameterAfterMergingTwoTrees_3203
+{
+private:
+    /* data */
+public:
+    int minimumDiameterAfterMerge(std::vector<std::vector<int>> &edges1, std::vector<std::vector<int>> &edges2)
+    {
+    }
+};
+int main()
+{
+    FindMinimumDiameterAfterMergingTwoTrees_3203 f1;
+    std::vector<std::vector<int>> edges1 = {};
+    std::vector<std::vector<int>> edges2 = {};
+    f1.minimumDiameterAfterMerge(edges1, edges2);
+    return 0;
+}
