b326560531a78e490c544eaa567c359f37b7acad
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-12-27 15:29:01 +0530

    BiggestSightseeingPair_1014.cpp done with TLE

--- BiggestSightseeingPair_1014.cpp
+++ BiggestSightseeingPair_1014.cpp
@@ -9,12 +9,21 @@ private:
 public:
     int maxScoreSightseeingPair(std::vector<int> &values)
     {
+        std::priority_queue<int> maxheap;
+        for (int i = 0; i < values.size(); i++)
+        {
+            for (int j = i + 1; j < values.size(); j++)
+            {
+                maxheap.push(values[i] + values[j] - (j - i));
+            }
+        }
+        return maxheap.top();
     }
 };
 int main()
 {
     BiggestSightseeingPair_1014 b1;
-    std::vector<int> values = {};
-    b1.maxScoreSightseeingPair(values);
+    std::vector<int> values = {8, 1, 5, 2, 6};
+    std::cout << b1.maxScoreSightseeingPair(values);
     return 0;
 }
