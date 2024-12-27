db2cea8c8fa2ef55691fc4a90ce72e76217d9786
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-12-27 15:11:37 +0530

    BiggestSightseeingPair_1014.cpp template is ready

---
+++ BiggestSightseeingPair_1014.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+class BiggestSightseeingPair_1014
+{
+private:
+    /* data */
+public:
+    int maxScoreSightseeingPair(std::vector<int> &values)
+    {
+    }
+};
+int main()
+{
+    BiggestSightseeingPair_1014 b1;
+    std::vector<int> values = {};
+    b1.maxScoreSightseeingPair(values);
+    return 0;
+}
