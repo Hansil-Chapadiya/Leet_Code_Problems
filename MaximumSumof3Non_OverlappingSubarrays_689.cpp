c68866e4d5daabd20007288c29359d1b5d5200e5
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-12-28 11:46:58 +0530

    MaximumSumof3Non_OverlappingSubarrays_689.cpp template is ready

---
+++ MaximumSumof3Non_OverlappingSubarrays_689.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+class MaximumSumof3Non_OverlappingSubarrays_689
+{
+private:
+    /* data */
+public:
+    std::vector<int> maxSumOfThreeSubarrays(std::vector<int> &nums, int k)
+    {
+    }
+};
+int main()
+{
+    std::vector<int> nums = {1,2,1,2,6,7,5,1};
+    MaximumSumof3Non_OverlappingSubarrays_689 m1;
+    m1.maxSumOfThreeSubarrays(nums, 2);
+    return 0;
+}
