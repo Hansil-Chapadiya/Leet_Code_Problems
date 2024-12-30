9321c1611dafe1556ab317d02339c29b237264f8
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-12-30 19:35:48 +0530

    CountWaysToBuildGoodStrings_2466.cpp template is read

---
+++ CountWaysToBuildGoodStrings_2466.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+class CountWaysToBuildGoodStrings_2466
+{
+private:
+    /* data */
+public:
+    int countGoodStrings(int low, int high, int zero, int one)
+    {
+    }
+};
+int main()
+{
+    CountWaysToBuildGoodStrings_2466 c1;
+    c1.countGoodStrings(3, 3, 1, 1);
+    return 0;
+}
