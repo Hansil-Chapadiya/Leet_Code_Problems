2e80ff2952207915d9efb96fd308b02fe89aad03
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-12-30 19:45:16 +0530

    CountWaysToBuildGoodStrings_2466.cpp done

--- CountWaysToBuildGoodStrings_2466.cpp
+++ CountWaysToBuildGoodStrings_2466.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <cmath>
+#include <vector>
 class CountWaysToBuildGoodStrings_2466
 {
 private:
@@ -6,11 +8,33 @@ private:
 public:
     int countGoodStrings(int low, int high, int zero, int one)
     {
+        long long mod = std::pow(10, 9) + 7;
+        std::vector<int> dp(high + 1, 0);
+        dp[0] = 1;
+
+        for (int i = 1; i <= high; i++)
+        {
+            if (i >= zero)
+            {
+                dp[i] = (dp[i] + dp[i - zero]) % mod;
+            }
+            if (i >= one)
+            {
+                dp[i] = (dp[i] + dp[i - one]) % mod;
+            }
+        }
+
+        int result = 0;
+        for (int i = low; i <= high; i++)
+        {
+            result = (result + dp[i]) % mod;
+        }
+        return result;
     }
 };
 int main()
 {
     CountWaysToBuildGoodStrings_2466 c1;
-    c1.countGoodStrings(3, 3, 1, 1);
+    std::cout<<c1.countGoodStrings(3, 3, 1, 1);
     return 0;
 }
