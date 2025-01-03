c5cec310f7b73c229290e652f86108aaf4acead3
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-01-03 08:25:37 +0530

    NumberOfWaystoSplitArray_2270.cpp template is ready

---
+++ NumberOfWaystoSplitArray_2270.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+class NumberOfWaystoSplitArray_2270
+{
+private:
+    /* data */
+public:
+    int waysToSplitArray(std::vector<int> &nums)
+    {
+    }
+};
+int main()
+{
+    std::vector<int> nums = {10, 4, -8, 7};
+    NumberOfWaystoSplitArray_2270 n1;
+    n1.waysToSplitArray(nums);
+    return 0;
+}
