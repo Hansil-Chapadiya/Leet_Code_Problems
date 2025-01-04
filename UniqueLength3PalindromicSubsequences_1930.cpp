5de7a305cc0f4567dee025f00ae60e3733344e70
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-01-04 09:37:52 +0530

    UniqueLength3PalindromicSubsequences_1930.cpp template is ready

---
+++ UniqueLength3PalindromicSubsequences_1930.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <string>
+class UniqueLength3PalindromicSubsequences_1930
+{
+private:
+public:
+    int countPalindromicSubsequence(std::string s)
+    {
+    }
+};
+int main()
+{
+    UniqueLength3PalindromicSubsequences_1930 u1;
+    u1.countPalindromicSubsequence("aabca");
+    return 0;
+}
