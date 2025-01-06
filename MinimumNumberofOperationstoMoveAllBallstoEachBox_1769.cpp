7fef9123e4a212982097d5cca8c5859dcb2ace47
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-01-06 19:27:07 +0530

    MinimumNumberofOperationstoMoveAllBallstoEachBox_1769.cpp done

--- MinimumNumberofOperationstoMoveAllBallstoEachBox_1769.cpp
+++ MinimumNumberofOperationstoMoveAllBallstoEachBox_1769.cpp
@@ -9,6 +9,31 @@ public:
     std::vector<int> minOperations(std::string boxes)
     {
         std::vector<int> answer(boxes.length(), 0);
+        int operation = 0, balls = 0;
+        for (int i = 0; i < boxes.length(); i++)
+        {
+            if (balls > 0)
+            {
+                operation += balls;
+            }
+            answer[i] = operation;
+            if (boxes[i] == '1')
+            {
+                balls++;
+                /* code */
+            }
+        }
+        operation = balls = 0;
+        for (int i = boxes.length() - 1; i >= 0; i--)
+        {
+            operation += balls;
+            if (boxes[i] == '1')
+            {
+                balls++;
+            }
+            answer[i] += operation;
+        }
+        return answer;
     }
 };
 int main()
