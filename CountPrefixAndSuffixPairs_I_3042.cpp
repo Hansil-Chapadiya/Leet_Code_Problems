b00978fcef5fce66eb2b1ff310de54128b77ce09
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-01-08 08:54:15 +0530

    template is ready

---
+++ CountPrefixAndSuffixPairs_I_3042.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+#include<string>
+class CountPrefixAndSuffixPairs_I_3042
+{
+private:
+    /* data */
+public:
+    int countPrefixSuffixPairs(std::vector<std::string>& words) {
+
+    }
+};
+int main()
+{
+    CountPrefixAndSuffixPairs_I_3042 c1;
+    std::vector<std::string> words = {"a","aba","ababa","aa"};
+    c1.countPrefixSuffixPairs(words);
+    return 0;
+}
