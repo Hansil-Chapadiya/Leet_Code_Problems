f37df233cd7560867229d099c36479bf6ed368e8
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-01-09 16:53:22 +0530

    done

--- CountingWordsWithGivenPrefix_2185.cpp
+++ CountingWordsWithGivenPrefix_2185.cpp
@@ -8,15 +8,21 @@ private:
 public:
     int prefixCount(std::vector<std::string> &words, std::string pref)
     {
+        int cnt = 0;
         for (auto word : words)
         {
+            if (word.compare(0, pref.length(), pref) == 0)
+            {
+                cnt++;
+            }
         }
+        return cnt;
     }
 };
 int main()
 {
     CountingWordsWithGivenPrefix_2185 c1;
     std::vector<std::string> words = {"pay", "attention", "practice", "attend"};
-    c1.prefixCount(words, "");
+    std::cout << c1.prefixCount(words, "at");
     return 0;
 }
