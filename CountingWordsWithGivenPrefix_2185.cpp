ae21e030d682571a41e459a90f66d0bb2054abee
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-01-09 16:48:10 +0530

    CountingWordsWithGivenPrefix_2185.cpp template is ready

---
+++ CountingWordsWithGivenPrefix_2185.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <string>
+class CountingWordsWithGivenPrefix_2185
+{
+private:
+    /* data */
+public:
+    int prefixCount(std::vector<std::string> &words, std::string pref)
+    {
+        for (auto word : words)
+        {
+        }
+    }
+};
+int main()
+{
+    CountingWordsWithGivenPrefix_2185 c1;
+    std::vector<std::string> words = {"pay", "attention", "practice", "attend"};
+    c1.prefixCount(words, "");
+    return 0;
+}
