78bb9a4dd080cffb6cfe6aa080228d6746395db0
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-01-11 08:03:01 +0530

    ConstructKPalindromeString_1400.cpp template is ready

---
+++ ConstructKPalindromeString_1400.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <string>
+class ConstructKPalindromeString_1400
+{
+private:
+    /* data */
+public:
+    bool canConstruct(std::string s, int k)
+    {
+    }
+};
+int main()
+{
+    ConstructKPalindromeString_1400 c1;
+    c1.canConstruct("", 3);
+    return 0;
+}
