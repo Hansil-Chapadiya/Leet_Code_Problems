a9aeaf491113d9f9c4778c1144eaf70f72111150
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-01-11 08:47:06 +0530

    ConstructKPalindromeString_1400.cpp done

--- ConstructKPalindromeString_1400.cpp
+++ ConstructKPalindromeString_1400.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <unordered_map>
 class ConstructKPalindromeString_1400
 {
 private:
@@ -8,11 +9,30 @@ private:
 public:
     bool canConstruct(std::string s, int k)
     {
+        if (s.length() < k)
+        {
+            return false;
+        }
+
+        std::unordered_map<char, int> freq_cnt;
+        for (auto c : s)
+        {
+            freq_cnt[c]++;
+        }
+        int cnt = 0;
+        for (auto c : freq_cnt)
+        {
+            if (c.second % 2 == 1)
+            {
+                cnt++;
+            }
+        }
+        return cnt > k ? false : true;
     }
 };
 int main()
 {
     ConstructKPalindromeString_1400 c1;
-    c1.canConstruct("", 3);
+    std::cout << c1.canConstruct("ab", 10);
     return 0;
 }
