e1b52da0f82ceb963feb16cdadc09a788d3ea92c
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-01-12 07:41:01 +0530

    template is ready

---
+++ CheckifParenthesesStringCanBeValid_2116.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <unordered_map>
+
+class CheckifParenthesesStringCanBeValid_2116
+{
+private:
+    /* data */
+public:
+    bool canBeValid(std::string s, std::string locked)
+    {
+    }
+};
+int main()
+{
+    CheckifParenthesesStringCanBeValid_2116 c1;
+    c1.canBeValid("))()))","010100");
+    return 0;
+}
