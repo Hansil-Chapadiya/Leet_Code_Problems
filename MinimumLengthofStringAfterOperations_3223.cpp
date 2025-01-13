a0a61b5a8848ad3e2e33ce31e3afbd0cfd38dccf
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-01-13 15:58:25 +0530

    done

--- MinimumLengthofStringAfterOperations_3223.cpp
+++ MinimumLengthofStringAfterOperations_3223.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <unordered_map>
+#include <numeric>
 class MinimumLengthofStringAfterOperations_3223
 {
 private:
@@ -8,12 +9,41 @@ private:
 public:
     int minimumLength(std::string s)
     {
-
+        std::unordered_map<char, int> u_map;
+        for (auto c : s)
+        {
+            u_map[c]++;
+        }
+        for (auto c : u_map)
+        {
+            std::cout << c.first << " " << c.second << " ";
+            if (c.second % 2 == 0)
+            {
+                while (c.second > 2)
+                {
+                    c.second--;
+                }
+            }
+            else
+            {
+                while (c.second > 1)
+                {
+                    c.second--;
+                }
+            }
+            u_map[c.first] = c.second;
+        }
+        return std::accumulate(
+            u_map.begin(), u_map.end(), 0,
+            [](int acc, const auto &pair)
+            {
+                return acc + pair.second; // Sum up the values
+            });
     }
 };
 int main()
 {
     MinimumLengthofStringAfterOperations_3223 m1;
-    m1.minimumLength("abaacbcbb");
+    std::cout << m1.minimumLength("abaacbcbb");
     return 0;
 }
