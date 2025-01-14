2985a02d2062f13a9c8512e2d61e09a242e98348
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-01-14 09:59:23 +0530

    FindthePrefixCommonArrayOfTwoArrays_2657.cpp tempalte is ready

---
+++ FindthePrefixCommonArrayOfTwoArrays_2657.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+class FindthePrefixCommonArrayOfTwoArrays_2657
+{
+private:
+    /* data */
+public:
+    std::vector<int> findThePrefixCommonArray(std::vector<int> &A, std::vector<int> &B)
+    {
+    }
+};
+int main()
+{
+    std::vector<int> A;
+    std::vector<int> B;
+    FindthePrefixCommonArrayOfTwoArrays_2657 f1;
+    f1.findThePrefixCommonArray(A, B);
+    return 0;
+}
