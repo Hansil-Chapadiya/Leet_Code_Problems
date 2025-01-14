9f6b2e9f87d12427c626e466e2ce018f03233f97
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-01-14 10:35:17 +0530

    done

--- FindthePrefixCommonArrayOfTwoArrays_2657.cpp
+++ FindthePrefixCommonArrayOfTwoArrays_2657.cpp
@@ -1,20 +1,54 @@
 #include <iostream>
 #include <vector>
-#include <unordered_map>
+#include <unordered_set>
+
 class FindthePrefixCommonArrayOfTwoArrays_2657
 {
-private:
-    /* data */
 public:
     std::vector<int> findThePrefixCommonArray(std::vector<int> &A, std::vector<int> &B)
     {
+        std::vector<int> prefixCommon(A.size(), 0);
+        std::unordered_set<int> seenA, seenB;
+
+        int commonCount = 0;
+
+        for (int i = 0; i < A.size(); i++)
+        {
+            // Add current elements to their respective sets
+            if (seenB.count(A[i]))
+            {
+                commonCount++;
+            }
+            seenA.insert(A[i]);
+
+            if (seenA.count(B[i]))
+            {
+                commonCount++;
+            }
+            seenB.insert(B[i]);
+
+            // Update prefix common array
+            prefixCommon[i] = commonCount;
+        }
+
+        return prefixCommon;
     }
 };
+
 int main()
 {
-    std::vector<int> A;
-    std::vector<int> B;
+    std::vector<int> A = {1, 3, 2, 4};
+    std::vector<int> B = {3, 1, 2, 4};
     FindthePrefixCommonArrayOfTwoArrays_2657 f1;
-    f1.findThePrefixCommonArray(A, B);
+    std::vector<int> result = f1.findThePrefixCommonArray(A, B);
+
+    // Output the result
+    std::cout << "Prefix Common Array: ";
+    for (int x : result)
+    {
+        std::cout << x << " ";
+    }
+    std::cout << std::endl;
+
     return 0;
 }
