cddef232987dc9b9b32bfd3ba03e7850e18092f7
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-01-16 19:33:07 +0530

    template is ready

---
+++ BitwiseXorOfAllPairing_2425.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+class BitwiseXorOfAllPairing_2425
+{
+private:
+    /* data */
+public:
+    int xorAllNums(std::vector<int> &nums1, std::vector<int> &nums2)
+    {
+    }
+};
+int main()
+{
+    BitwiseXorOfAllPairing_2425 b1;
+    std::vector<int> nums1 = {2, 1, 3};
+    std::vector<int> nums2 = {10, 2, 5, 0};
+    b1.xorAllNums(nums1, nums2);
+    return 0;
+}
+static const int hansil = []()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return 0;
+}();
