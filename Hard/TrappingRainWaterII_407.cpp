ce26b0ac1c9fee386134aa49e902b8387275d1f3
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-01-19 10:35:50 +0530

    template is ready

---
+++ Hard/TrappingRainWaterII_407.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+class TrappingRainWaterII_407
+{
+private:
+    /* data */
+public:
+    int trapRainWater(std::vector<std::vector<int>> &heightMap)
+    {
+    }
+};
+int main()
+{
+    TrappingRainWaterII_407 t1;
+    std::vector<std::vector<int>> heightMap = {{1, 4, 3, 1, 3, 2}, {3, 2, 1, 3, 2, 4}, {2, 3, 3, 2, 3, 1}};
+    t1.trapRainWater(heightMap);
+}
