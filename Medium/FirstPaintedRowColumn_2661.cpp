08d3ee48c40d7fb221219ca061b67f663997cb89
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-01-20 19:05:22 +0530

    template is ready

---
+++ Medium/FirstPaintedRowColumn_2661.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+class FirstPaintedRowColumn_2661
+{
+private:
+    /* data */
+public:
+    int firstCompleteIndex(std::vector<int> &arr, std::vector<std::vector<int>> &mat)
+    {
+    }
+};
+int main()
+{
+    FirstPaintedRowColumn_2661 f1;
+    std::vector<int> arr = {1, 3, 4, 2};
+    std::vector<std::vector<int>> mat = {{1, 4}, {2, 3}};
+    f1.firstCompleteIndex(arr, mat);
+    return 0;
+}
