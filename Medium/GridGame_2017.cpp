5b63d3458af557a23bbaaa771c874a1e51a3da34
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-01-21 13:39:50 +0530

    done

--- Medium/GridGame_2017.cpp
+++ Medium/GridGame_2017.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <numeric>
+#include <climits>
 class GridGame_2017
 {
 private:
@@ -7,11 +9,25 @@ private:
 public:
     long long gridGame(std::vector<std::vector<int>> &grid)
     {
+        long long first_row_sum = std::accumulate(grid[0].begin(), grid[0].end(), 0);
+        long long second_row_sum = 0;
+
+        long long minimum_sum = LONG_MAX;
+
+        for (int i = 0; i < grid[0].size(); i++)
+        {
+            first_row_sum -= grid[0][i];
+
+            minimum_sum = std::min(minimum_sum, std::max(first_row_sum, second_row_sum));
+            second_row_sum += grid[1][i];
+        }
+        return minimum_sum;
     }
 };
 int main()
 {
     GridGame_2017 g1;
     std::vector<std::vector<int>> grid = {{2, 5, 4}, {1, 5, 1}};
+    std::cout << g1.gridGame(grid);
     return 0;
 }
