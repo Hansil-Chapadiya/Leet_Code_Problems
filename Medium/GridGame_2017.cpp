565740c16a1c12fe1c7c231da394054e2e56bcef
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-01-21 13:22:18 +0530

    template is ready

---
+++ Medium/GridGame_2017.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+class GridGame_2017
+{
+private:
+    /* data */
+public:
+    long long gridGame(std::vector<std::vector<int>> &grid)
+    {
+    }
+};
+int main()
+{
+    GridGame_2017 g1;
+    std::vector<std::vector<int>> grid = {{2, 5, 4}, {1, 5, 1}};
+    return 0;
+}
