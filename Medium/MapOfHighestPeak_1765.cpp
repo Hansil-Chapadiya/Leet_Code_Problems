0a62f416702ddaa4b65e1549251626ac7298ad4c
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-01-22 11:13:24 +0530

    Medium/MapOfHighestPeak_1765.cpp template is ready

---
+++ Medium/MapOfHighestPeak_1765.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+class MapOfHighestPeak_1765
+{
+private:
+    /* data */
+public:
+    std::vector<std::vector<int>> highestPeak(std::vector<std::vector<int>> &isWater)
+    {
+    }
+};
+int main()
+{
+    MapOfHighestPeak_1765 m1;
+    std::vector<std::vector<int>> isWater = {{0, 1}, {0, 0}};
+    m1.highestPeak(isWater);
+    return 0;
+}
