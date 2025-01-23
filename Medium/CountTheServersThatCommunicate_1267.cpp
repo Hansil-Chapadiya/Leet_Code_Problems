2d8cadea46a99b19a4cba2700cca4d60638a112c
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-01-23 17:36:01 +0530

    Medium/CountTheServersThatCommunicate_1267.cpp templaTE is ready

---
+++ Medium/CountTheServersThatCommunicate_1267.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+class CountTheServersThatCommunicate_1267
+{
+private:
+    /* data */
+public:
+    int countServers(std::vector<std::vector<int>> &grid)
+    {
+    }
+};
+int main()
+{
+    std::vector<std::vector<int>> grid = {{1, 0}, {0, 1}};
+    CountTheServersThatCommunicate_1267 c1;
+    c1.countServers(grid);
+    return 0;
+}
