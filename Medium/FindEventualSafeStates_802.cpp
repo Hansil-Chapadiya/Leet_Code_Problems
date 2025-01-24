a209e10daa8e2081ed83bb2e08a314efbe75cfbd
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-01-24 17:38:35 +0530

    template is ready

---
+++ Medium/FindEventualSafeStates_802.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+class FindEventualSafeStates_802
+{
+private:
+    /* data */
+public:
+    std::vector<int> eventualSafeNodes(std::vector<std::vector<int>> &graph)
+    {
+    }
+};
+int main()
+{
+    FindEventualSafeStates_802 f1;
+    std::vector<std::vector<int>> graph = {{1, 2}, {2, 3}, {5}, {0}, {5}, {}, {}};
+    return 0;
+}
