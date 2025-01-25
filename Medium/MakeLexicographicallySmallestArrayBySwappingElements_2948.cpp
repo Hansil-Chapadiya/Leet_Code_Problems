eb6c54bdeca7b159500002c7edeb8b06efcce799
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-01-25 18:42:19 +0530

    Medium/MakeLexicographicallySmallestArrayBySwappingElements_2948.cpp template is ready

---
+++ Medium/MakeLexicographicallySmallestArrayBySwappingElements_2948.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+class MakeLexicographicallySmallestArrayBySwappingElements_2948
+{
+private:
+public:
+    std::vector<int> lexicographicallySmallestArray(std::vector<int> &nums, int limit)
+    {
+    }
+};
+int main()
+{
+    std::vector<int> nums = {1,5,3,9,8};
+    MakeLexicographicallySmallestArrayBySwappingElements_2948 m1;
+    m1.lexicographicallySmallestArray(nums, 9);
+    return 0;
+}
