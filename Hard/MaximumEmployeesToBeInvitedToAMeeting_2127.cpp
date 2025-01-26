232162cc6ca456225be86b4617f8849e5b1f974d
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-01-26 19:33:46 +0530

    Hard/MaximumEmployeesToBeInvitedToAMeeting_2127.cpp template is ready

---
+++ Hard/MaximumEmployeesToBeInvitedToAMeeting_2127.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+class MaximumEmployeesToBeInvitedToAMeeting_2127
+{
+private:
+public:
+    int maximumInvitations(std::vector<int> &favorite)
+    {
+    }
+};
+int main()
+{
+    return 0;
+}
