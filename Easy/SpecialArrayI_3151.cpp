6bd7911ba464b404f084b5d4b50ef3b5c7a4b9e6
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-02-01 11:23:58 +0530

    done

--- Easy/SpecialArrayI_3151.cpp
+++ Easy/SpecialArrayI_3151.cpp
@@ -7,20 +7,35 @@ private:
     {
         if (n1 % 2 == 1)
         {
-            return true;
+            return 1;
         }
-        return false;
+        return -1;
     }
 
 public:
     bool isArraySpecial(std::vector<int> &nums)
     {
+        if (nums.size() == 1)
+        {
+            return true;
+        }
 
+        for (int i = 1; i < nums.size(); i++)
+        {
+            if (isOdd(nums[i]) == isOdd(nums[i - 1]))
+            {
+                return false;
+            }
+        }
+        return true;
     }
 };
 int main()
 {
     SpecialArrayI_3151 s1;
-    std::vector<int> nums = {};
-    s1.isArraySpecial(nums);
+    // std::vector<int> nums = {1};
+    // std::vector<int> nums = {2,1,4};
+    // std::vector<int> nums = {4, 3, 1, 6};
+    std::vector<int> nums = {2,10};
+    std::cout << s1.isArraySpecial(nums);
 }
