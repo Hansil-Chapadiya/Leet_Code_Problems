82aa4b4ff026485f8b8a0a073a0cdde9c6a47322
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-02-02 09:40:08 +0530

    template is ready

---
+++ Easy/CheckIfArraySortedAndRotated_1752.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+class CheckIfArraySortedAndRotated_1752
+{
+private:
+    /* data */
+public:
+    bool check(std::vector<int> &nums)
+    {
+    }
+};
+int main()
+{
+    CheckIfArraySortedAndRotated_1752 c1;
+    std::vector<int> nums = {3, 4, 5, 1, 2};
+    c1.check(nums);
+    return 0;
+}
