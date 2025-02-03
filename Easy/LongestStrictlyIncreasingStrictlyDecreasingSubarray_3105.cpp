5bc90438b95a4a45c31de8999967035d3bca88b1
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-02-03 11:16:33 +0530

    template is ready

---
+++ Easy/LongestStrictlyIncreasingStrictlyDecreasingSubarray_3105.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+class LongestStrictlyIncreasingStrictlyDecreasingSubarray_3105
+{
+private:
+    /* data */
+public:
+    int longestMonotonicSubarray(std::vector<int> &nums)
+    {
+    }
+};
+int main()
+{
+    LongestStrictlyIncreasingStrictlyDecreasingSubarray_3105 l1;
+    std::vector<int> nums = {1,4,3,3,2};
+    l1.longestMonotonicSubarray(nums);
+    return 0;
+}
