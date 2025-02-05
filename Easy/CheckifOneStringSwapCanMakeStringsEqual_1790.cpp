078f163c6a4705bf41272e023c809fb79ea5d743
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-02-05 17:05:10 +0530

    Easy/CheckifOneStringSwapCanMakeStringsEqual_1790.cpp template is ready

---
+++ Easy/CheckifOneStringSwapCanMakeStringsEqual_1790.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+class CheckifOneStringSwapCanMakeStringsEqual_1790
+{
+private:
+
+public:
+    bool areAlmostEqual(std::string s1, std::string s2)
+    {
+    }
+};
+int main()
+{
+    CheckifOneStringSwapCanMakeStringsEqual_1790 c1;
+    std::cout << c1.areAlmostEqual("bank", "kanb");
+}
