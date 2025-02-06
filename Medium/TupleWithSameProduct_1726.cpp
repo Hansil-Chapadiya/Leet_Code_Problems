1f7abf8fc17bc40b69b9b4284e61190dd9622dad
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-02-06 07:41:50 +0530

    Medium/TupleWithSameProduct_1726.cpp template is ready

---
+++ Medium/TupleWithSameProduct_1726.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+class TupleWithSameProduct_1726
+{
+private:
+    /* data */
+public:
+    int tupleSameProduct(std::vector<int> &nums)
+    {
+    }
+};
+int main()
+{
+    TupleWithSameProduct_1726 t1;
+    std::vector<int> nums = {2, 3, 4, 6};
+    t1.tupleSameProduct(nums);
+    return 0;
+}
