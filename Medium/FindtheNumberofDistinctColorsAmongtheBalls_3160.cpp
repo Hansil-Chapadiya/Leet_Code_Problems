0c17359522585b930671542e8f7a5fe865da307b
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-02-07 08:21:54 +0530

    template is ready

---
+++ Medium/FindtheNumberofDistinctColorsAmongtheBalls_3160.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+class FindtheNumberofDistinctColorsAmongtheBalls_3160
+{
+private:
+
+public:
+    std::vector<int> queryResults(int limit, std::vector<std::vector<int>>& queries) {
+
+    }
+};
+int main()
+{
+    FindtheNumberofDistinctColorsAmongtheBalls_3160 f1;
+    std::vector<std::vector<int>> queries = {{1, 4},
+                                             {2, 5},
+                                             {1, 3},
+                                             {3, 4}};
+    return 0;
+}
