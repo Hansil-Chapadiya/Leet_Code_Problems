3517bfe529823fc0260f8ea7959155bcd08b0d77
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-02-08 08:20:13 +0530

    DesignNumberContainerSystem_2349.cpp done

--- Medium/DesignNumberContainerSystem_2349.cpp
+++ Medium/DesignNumberContainerSystem_2349.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+/* #include <iostream>
 #include <vector>
 #include <unordered_map>
 #include <climits>
@@ -45,9 +45,56 @@ int main()
     return 0;
 }
 
-/**
- * Your NumberContainers object will be instantiated and called as such:
- * NumberContainers* obj = new NumberContainers();
- * obj->change(index,number);
- * int param_2 = obj->find(number);
  */
+
+#include <iostream>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include <climits>
+
+class DesignNumberContainerSystem_2349
+{
+private:
+    std::unordered_map<int, int> index_map;  // Maps index -> number
+    std::map<int, std::set<int>> number_map; // Maps number -> sorted set of indices
+
+public:
+    DesignNumberContainerSystem_2349() {}
+
+    void change(int index, int number)
+    {
+        if (index_map.find(index) != index_map.end())
+        {
+            int old_number = index_map[index];
+            number_map[old_number].erase(index);
+            if (number_map[old_number].empty())
+            {
+                number_map.erase(old_number);
+            }
+        }
+        index_map[index] = number;
+        number_map[number].insert(index);
+    }
+
+    int find(int number)
+    {
+        if (number_map.find(number) == number_map.end())
+            return -1;
+        return *(number_map[number].begin()); // Get the smallest index
+    }
+};
+
+int main()
+{
+    DesignNumberContainerSystem_2349 *d1 = new DesignNumberContainerSystem_2349();
+    std::cout << d1->find(10) << std::endl; // Output: -1
+    d1->change(2, 10);
+    d1->change(1, 10);
+    d1->change(3, 10);
+    d1->change(5, 10);
+    std::cout << d1->find(10) << std::endl; // Output: 1
+    d1->change(1, 20);
+    std::cout << d1->find(10) << std::endl; // Output: 2
+    return 0;
+}
