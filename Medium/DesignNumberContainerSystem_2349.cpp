d3712928037d0f95822c826c6d7ced91b0d43079
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-02-08 08:13:07 +0530

    TLE approach

--- Medium/DesignNumberContainerSystem_2349.cpp
+++ Medium/DesignNumberContainerSystem_2349.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include <vector>
+#include <unordered_map>
+#include <climits>
 class DesignNumberContainerSystem_2349
 {
 private:
-    /* data */
+    std::unordered_map<int, int> u_map;
+
 public:
     DesignNumberContainerSystem_2349()
     {
@@ -11,17 +14,34 @@ public:
 
     void change(int index, int number)
     {
+        u_map[index] = number;
     }
 
     int find(int number)
     {
+        int min = INT_MAX;
+
+        for (auto i : u_map)
+        {
+            if (i.second == number && i.first < min)
+            {
+                min = i.first;
+            }
+        }
+        return min == INT_MAX ? -1 : min;
     }
 };
 int main()
 {
-    DesignNumberContainerSystem_2349* d1 = new DesignNumberContainerSystem_2349();
-    d1->change(1,7);
-    int param_2 = d1->find(10);
+    DesignNumberContainerSystem_2349 *d1 = new DesignNumberContainerSystem_2349();
+    std::cout << d1->find(10) << std::endl;
+    d1->change(2, 10);
+    d1->change(1, 10);
+    d1->change(3, 10);
+    d1->change(5, 10);
+    std::cout << d1->find(10) << std::endl;
+    d1->change(1, 20);
+    std::cout << d1->find(10) << std::endl;
     return 0;
 }
 
