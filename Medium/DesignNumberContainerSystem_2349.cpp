92de4a11d0eba905f800ba40824e45728862fd25
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-02-08 07:43:28 +0530

    Medium/DesignNumberContainerSystem_2349.cpp template is ready

---
+++ Medium/DesignNumberContainerSystem_2349.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+class DesignNumberContainerSystem_2349
+{
+private:
+    /* data */
+public:
+    DesignNumberContainerSystem_2349()
+    {
+    }
+
+    void change(int index, int number)
+    {
+    }
+
+    int find(int number)
+    {
+    }
+};
+int main()
+{
+    DesignNumberContainerSystem_2349* d1 = new DesignNumberContainerSystem_2349();
+    d1->change(1,7);
+    int param_2 = d1->find(10);
+    return 0;
+}
+
+/**
+ * Your NumberContainers object will be instantiated and called as such:
+ * NumberContainers* obj = new NumberContainers();
+ * obj->change(index,number);
+ * int param_2 = obj->find(number);
+ */
