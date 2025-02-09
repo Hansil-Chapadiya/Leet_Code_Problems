2f3e24886543796e0084a6f49c4e005dedc2b36d
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-02-09 20:55:18 +0530

    template is ready

---
+++ Easy/PlusOne_66.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <string>
+class PlusOne_66
+{
+private:
+public:
+    std::vector<int> plusOne(std::vector<int> &digits)
+    {
+    }
+};
+int main()
+{
+    PlusOne_66 p1;
+    std::vector<int> nums = {1, 2, 3};
+    p1.plusOne(nums);
+    return 0;
+}
