b7e78e8b11a9003d211316741f578cfac70717ab
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-02-09 08:05:35 +0530

    Medium/CountTheNumberOfBadPairs_2363.cpp template is ready

---
+++ Medium/CountTheNumberOfBadPairs_2363.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+class CountTheNumberOfBadPairs_2363
+{
+private:
+public:
+    long long countBadPairs(std::vector<int> &nums)
+    {
+    }
+};
+int main()
+{
+    CountTheNumberOfBadPairs_2363 c1;
+    std::vector<int> nums = {4, 1, 3, 3};
+    c1.countBadPairs(nums);
+    return 0;
+}
