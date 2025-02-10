46da6a80976ae77dac0bd084ce3d8865e44aff4d
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-02-10 07:56:40 +0530

    template is ready

---
+++ Easy/ClearDigit_3174.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <string>
+class ClearDigit_3174
+{
+private:
+    /* data */
+public:
+    std::string clearDigits(std::string s)
+    {
+    }
+};
+int main()
+{
+    ClearDigit_3174 c1;
+    c1.clearDigits("abc");
+    return 0;
+}
