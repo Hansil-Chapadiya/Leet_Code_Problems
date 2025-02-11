993d9678df24bf3a3bf6a3c8bad226bffc612dd9
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-02-11 08:04:13 +0530

    RemoveAllOccuranceOfSubstring_1910.cpp done

--- Medium/RemoveAllOccuranceOfSubstring_1910.cpp
+++ Medium/RemoveAllOccuranceOfSubstring_1910.cpp
@@ -4,14 +4,25 @@
 class RemoveAllOccuranceOfSubstring_1910
 {
 private:
-
 public:
     std::string removeOccurrences(std::string s, std::string part)
     {
+        int i;
+        while (true)
+        {
+            if ((i = s.find(part)) != std::string::npos)
+            {
+                s = s.erase(i, part.size());
+            }
+            else
+            {
+                return s;
+            }
+        }
     }
 };
 int main()
 {
     RemoveAllOccuranceOfSubstring_1910 r1;
-    std::cout<< r1.removeOccurrences("daabcbaabcbc","abc");
+    std::cout << r1.removeOccurrences("daabcbaabcbc", "abc");
 }
