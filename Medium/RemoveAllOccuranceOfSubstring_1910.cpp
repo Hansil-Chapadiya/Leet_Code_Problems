961169c5037259625594999de1cc6933e7d3e714
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-02-11 08:40:57 +0530

    stack based approach

--- Medium/RemoveAllOccuranceOfSubstring_1910.cpp
+++ Medium/RemoveAllOccuranceOfSubstring_1910.cpp
@@ -1,13 +1,36 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <stack>
 class RemoveAllOccuranceOfSubstring_1910
 {
 private:
+    std::stack<char> stk;
+
 public:
     std::string removeOccurrences(std::string s, std::string part)
     {
-        int i;
+        std::string stack;  // Use a string as a stack
+        int partLen = part.size();
+
+        for (char c : s) {
+            stack.push_back(c);  // Push character to stack
+
+            // Check if the last 'partLen' characters in stack match 'part'
+            if (stack.size() >= partLen && stack.substr(stack.size() - partLen) == part) {
+                stack.erase(stack.end() - partLen, stack.end());  // Remove last 'partLen' characters
+            }
+        }
+        return stack;
+    }
+};
+int main()
+{
+    RemoveAllOccuranceOfSubstring_1910 r1;
+    std::cout << r1.removeOccurrences("daabcbaabcbc", "abc");
+}
+/*
+int i;
         while (true)
         {
             if ((i = s.find(part)) != std::string::npos)
@@ -19,10 +42,5 @@ public:
                 return s;
             }
         }
-    }
-};
-int main()
-{
-    RemoveAllOccuranceOfSubstring_1910 r1;
-    std::cout << r1.removeOccurrences("daabcbaabcbc", "abc");
-}
+
+*/
