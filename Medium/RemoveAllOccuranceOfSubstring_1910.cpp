92aa0ad7d4924982dcfe7226dc46db5c4ca0c935
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-02-11 07:50:39 +0530

    Medium/RemoveAllOccuranceOfSubstring_1910.cpp template is ready

---
+++ Medium/RemoveAllOccuranceOfSubstring_1910.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <string>
+class RemoveAllOccuranceOfSubstring_1910
+{
+private:
+
+public:
+    std::string removeOccurrences(std::string s, std::string part)
+    {
+    }
+};
+int main()
+{
+    RemoveAllOccuranceOfSubstring_1910 r1;
+    std::cout<< r1.removeOccurrences("daabcbaabcbc","abc");
+}
