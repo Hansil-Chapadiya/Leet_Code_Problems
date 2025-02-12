04a0f1a26839134318a4d3e4e0039853ffce0af5
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-02-12 07:55:33 +0530

    template is ready

---
+++ Medium/MaxSumOfPairWithEqualSumDigits_2342.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+class MaxSumOfPairWithEqualSumDigits_2342
+{
+private:
+
+public:
+    int maximumSum(std::vector<int> &nums)
+    {
+    }
+};
+int main()
+{
+    MaxSumOfPairWithEqualSumDigits_2342 m1;
+    std::vector<int> nums = {};
+    m1.maximumSum(nums);
+    return 0;
+}
