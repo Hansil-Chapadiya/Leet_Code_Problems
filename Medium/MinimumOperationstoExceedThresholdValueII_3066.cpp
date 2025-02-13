56732a18d11679c3638b223b17d684ac3daf6b2a
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-02-13 07:56:02 +0530

    Medium/MinimumOperationstoExceedThresholdValueII_3066.cpp template is ready

---
+++ Medium/MinimumOperationstoExceedThresholdValueII_3066.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+class MinimumOperationstoExceedThresholdValueII_3066
+{
+private:
+public:
+    int minOperations(std::vector<int> &nums, int k)
+    {
+    }
+};
+int main()
+{
+    std::vector<int> nums = {};
+    MinimumOperationstoExceedThresholdValueII_3066 m1;
+    m1.minOperations(nums, 1);
+    return 0;
+}
