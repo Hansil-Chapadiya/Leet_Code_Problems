52b2ca9681740f22adb213eae9d40e9e2ee4a425
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-02-14 09:21:15 +0530

    unsigned error approach

--- Medium/ProductoftheLastKNumbers_1352.cpp
+++ Medium/ProductoftheLastKNumbers_1352.cpp
@@ -4,6 +4,9 @@
 class ProductoftheLastKNumbers_1352
 {
 private:
+    int product = 1;
+    std::vector<int> intStream;
+
 public:
     ProductoftheLastKNumbers_1352()
     {
@@ -11,18 +14,53 @@ public:
 
     void add(int num)
     {
+        if (num != 0)
+        {
+            this->product = this->product * num;
+            this->intStream.push_back(product);
+        }
+        else
+        {
+            intStream.clear();
+            this->product = 1;
+        }
     }
 
     int getProduct(int k)
     {
+        int size = this->intStream.size();
+        if (k > size) // If k is greater than available numbers, return 0
+            return 0;
+        if (k == size) // If k equals the entire size, return the last stored product
+            return intStream.back();
+
+        return intStream.back() / intStream[size - k - 1]; // Compute the product of the last k numbers
+    }
+
+    void display()
+    {
+        for (auto i : intStream)
+        {
+            std::cout << i << " ";
+        }
     }
 };
 int main()
 {
-    ProductoftheLastKNumbers_1352 p1;
+    ProductoftheLastKNumbers_1352 *p1 = new ProductoftheLastKNumbers_1352();
+    p1->add(3);
+    p1->add(0);
+    p1->add(2);
+    p1->add(5);
+    p1->add(4);
+    std::cout << p1->getProduct(2);
+    // p1->getProduct(3);
+    // p1->getProduct(4);
+    // p1->add(8);
+    // p1->getProduct(2);
+    p1->display();
     return 0;
 }
-
 
 /**
  * Your ProductOfNumbers object will be instantiated and called as such:
@@ -30,3 +68,6 @@ int main()
  * obj->add(num);
  * int param_2 = obj->getProduct(k);
  */
+
+
+
