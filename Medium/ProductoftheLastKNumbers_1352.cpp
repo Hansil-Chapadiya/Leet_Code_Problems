49e3ba41704984a1c9c110e1a0e5d9b2ed516fbf
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-02-14 07:52:27 +0530

    Medium/ProductoftheLastKNumbers_1352.cpp

---
+++ Medium/ProductoftheLastKNumbers_1352.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+
+class ProductoftheLastKNumbers_1352
+{
+private:
+public:
+    ProductoftheLastKNumbers_1352()
+    {
+    }
+
+    void add(int num)
+    {
+    }
+
+    int getProduct(int k)
+    {
+    }
+};
+int main()
+{
+    ProductoftheLastKNumbers_1352 p1;
+    return 0;
+}
+
+
+/**
+ * Your ProductOfNumbers object will be instantiated and called as such:
+ * ProductOfNumbers* obj = new ProductOfNumbers();
+ * obj->add(num);
+ * int param_2 = obj->getProduct(k);
+ */
