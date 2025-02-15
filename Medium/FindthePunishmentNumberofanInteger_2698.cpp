dd3ba467224f6897b656040ca23a23e913612ece
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-02-15 07:50:15 +0530

    template is ready

---
+++ Medium/FindthePunishmentNumberofanInteger_2698.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+class FindthePunishmentNumberofanInteger_2698
+{
+private:
+public:
+    int punishmentNumber(int n)
+    {
+    }
+};
+int main()
+{
+    FindthePunishmentNumberofanInteger_2698 f1;
+    std::cout << f1.punishmentNumber(8);
+    return 0;
+}
