43007e91d529218f8d16e211d4816fdd775e6403
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-02-16 06:33:30 +0530

    template is ready

---
+++ Medium/ConstructtheLexicographicallyLargestValidSequence_1718.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+class ConstructtheLexicographicallyLargestValidSequence_1718
+{
+private:
+
+public:
+    std::vector<int> constructDistancedSequence(int n)
+    {
+    }
+};
+int main()
+{
+    ConstructtheLexicographicallyLargestValidSequence_1718 c1;
+    std::vector<int> answer = c1.constructDistancedSequence(9);
+    for (int i : answer)
+    {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
+}
