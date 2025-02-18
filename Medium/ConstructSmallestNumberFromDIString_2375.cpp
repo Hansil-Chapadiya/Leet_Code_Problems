be16afd50fcc75ce5437140d6407f6cdbb82a1d1
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-02-18 08:17:07 +0530

    Template is ready

---
+++ Medium/ConstructSmallestNumberFromDIString_2375.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+class ConstructSmallestNumberFromDIString_2375
+{
+private:
+public:
+    std::string smallestNumber(std::string pattern)
+    {
+    }
+};
+int main()
+{
+    ConstructSmallestNumberFromDIString_2375 c1;
+    std::cout << c1.smallestNumber("");
+}
