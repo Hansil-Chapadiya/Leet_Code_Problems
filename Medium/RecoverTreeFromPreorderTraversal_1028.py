a960153d2019827109a46705870a317c2ffc74a2
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-02-22 07:35:58 +0530

    Medium/RecoverTreeFromPreorderTraversal_1028.py template is ready

---
+++ Medium/RecoverTreeFromPreorderTraversal_1028.py
@@ -0,0 +1,13 @@
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, right=None, left=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class RecoverTreeFromPreorderTraversal_1028:
+    def recoverFromPreorder(self, traversal: str) -> Optional[TreeNode]:
+        pass
