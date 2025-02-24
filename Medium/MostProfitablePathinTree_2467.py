1c10e5f45b601e5ecb287287b033a4229bcd4b97
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-02-24 08:05:30 +0530

    template is reacy

---
+++ Medium/MostProfitablePathinTree_2467.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class MostProfitablePathinTree:
+    def mostProfitablePath(
+        self, edges: List[List[int]], bob: int, amount: List[int]
+    ) -> int:
+        return 0
+
+
+obj = MostProfitablePathinTree()
+obj.mostProfitablePath([[0, 1], [1, 2], [1, 3], [3, 4]], 3, [-2, 4, 2, -4, 6])
