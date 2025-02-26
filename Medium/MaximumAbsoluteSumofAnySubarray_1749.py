fa25f31e426ef0a131c1a40a7b562c45fd6f7051
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-02-26 10:53:19 +0530

    template is raedy

---
+++ Medium/MaximumAbsoluteSumofAnySubarray_1749.py
@@ -0,0 +1,8 @@
+from typing import List
+class MaximumAbsoluteSumofAnySubarray_1749:
+    def maxAbsoluteSum(self, nums: List[int]) -> int:
+        return 0
+
+
+obj = MaximumAbsoluteSumofAnySubarray_1749()
+obj.maxAbsoluteSum([1,-3,2,3,-4])
