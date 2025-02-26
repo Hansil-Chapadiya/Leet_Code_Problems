aa2c3ede05c338de2a06cfb6b78ac3a9377a6cde
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-02-26 11:59:05 +0530

    another approach

--- Medium/MaximumAbsoluteSumofAnySubarray_1749.py
+++ Medium/MaximumAbsoluteSumofAnySubarray_1749.py
@@ -1,26 +1,30 @@
+from itertools import accumulate
 from typing import List
 
 
 class MaximumAbsoluteSumofAnySubarray_1749:
     def maxAbsoluteSum(self, nums: List[int]) -> int:
-        maxSum = float("-inf")
-        minSum = float("inf")
-        currentSum = 0
-        for num in nums:
-            currentSum += num
-            maxSum = max(maxSum, currentSum)
-            if currentSum < 0:
-                currentSum = 0
+        # maxSum = float("-inf")
+        # minSum = float("inf")
+        # currentSum = 0
+        # for num in nums:
+        #     currentSum += num
+        #     maxSum = max(maxSum, currentSum)
+        #     if currentSum < 0:
+        #         currentSum = 0
 
-        currentSum = 0
-        for num in nums:
-            currentSum += num
-            minSum = min(minSum, currentSum)
-            if currentSum > 0:
-                currentSum = 0
-        return max(maxSum, abs(minSum)) # type: ignore
+        # currentSum = 0
+        # for num in nums:
+        #     currentSum += num
+        #     minSum = min(minSum, currentSum)
+        #     if currentSum > 0:
+        #         currentSum = 0
+        # return max(maxSum, abs(minSum)) # type: ignore
+
+        s = list(accumulate(nums, initial=0))
+        return max(s) - min(s)
 
 
 obj = MaximumAbsoluteSumofAnySubarray_1749()
 # print(obj.maxAbsoluteSum([1, -3, 2, 3, -4]))
-print(obj.maxAbsoluteSum([2,-5,1,-4,3,-2]))
+print(obj.maxAbsoluteSum([2, -5, 1, -4, 3, -2]))
