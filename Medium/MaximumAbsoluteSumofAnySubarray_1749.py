9e51f8a033079df900d50a7a0afd40730c9e50d5
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-02-26 11:58:08 +0530

    doen

--- Medium/MaximumAbsoluteSumofAnySubarray_1749.py
+++ Medium/MaximumAbsoluteSumofAnySubarray_1749.py
@@ -1,8 +1,26 @@
 from typing import List
+
+
 class MaximumAbsoluteSumofAnySubarray_1749:
     def maxAbsoluteSum(self, nums: List[int]) -> int:
-        return 0
+        maxSum = float("-inf")
+        minSum = float("inf")
+        currentSum = 0
+        for num in nums:
+            currentSum += num
+            maxSum = max(maxSum, currentSum)
+            if currentSum < 0:
+                currentSum = 0
+
+        currentSum = 0
+        for num in nums:
+            currentSum += num
+            minSum = min(minSum, currentSum)
+            if currentSum > 0:
+                currentSum = 0
+        return max(maxSum, abs(minSum)) # type: ignore
 
 
 obj = MaximumAbsoluteSumofAnySubarray_1749()
-obj.maxAbsoluteSum([1,-3,2,3,-4])
+# print(obj.maxAbsoluteSum([1, -3, 2, 3, -4]))
+print(obj.maxAbsoluteSum([2,-5,1,-4,3,-2]))
