0f2cf41f80142abcdb19585200ee7e049880b26c
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-02-27 22:19:28 +0530

    done

--- Easy/RemoveElement_27.py
+++ Easy/RemoveElement_27.py
@@ -1,7 +1,14 @@
 from typing import List
+
+
 class RemoveElement:
-     def removeElement(self, nums: List[int], val: int) -> int:
-        return 0
+    def removeElement(self, nums: List[int], val: int) -> int:
+        # result.remove(val)
+
+        while val in nums:
+            nums.remove(val)
+        return len(nums)
+
 
 obj = RemoveElement()
-print(obj.removeElement([3,2,2,3],3))
+print(obj.removeElement([3, 2, 2, 3], 3))
