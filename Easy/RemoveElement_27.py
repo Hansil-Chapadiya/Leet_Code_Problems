d7418d88560bc86d8952d19587972cd230471c0c
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-02-27 22:03:29 +0530

    RemoveElement_27.py template is ready

---
+++ Easy/RemoveElement_27.py
@@ -0,0 +1,7 @@
+from typing import List
+class RemoveElement:
+     def removeElement(self, nums: List[int], val: int) -> int:
+        return 0
+
+obj = RemoveElement()
+print(obj.removeElement([3,2,2,3],3))
