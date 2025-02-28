96f24fbdf8eae3f9fa40584efc7e0866c500542a
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-02-28 09:15:05 +0530

    template is ready

---
+++ Medium/ShortestCommonSubSequence_1092.py
@@ -0,0 +1,6 @@
+class ShortestCommonSubSequence_1092:
+    def shortestCommonSupersequence(self, str1: str, str2: str) -> str:
+        return ""
+
+obj = ShortestCommonSubSequence_1092()
+print(obj.shortestCommonSupersequence("abac","cab"))
