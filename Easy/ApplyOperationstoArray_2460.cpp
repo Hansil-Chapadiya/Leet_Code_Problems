74d24e08e4944ebe9df4e361bba20315579fe40d
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-03-01 20:53:21 +0530

    Easy/ApplyOperationstoArray_2460.cpp template is ready

---
+++ Easy/ApplyOperationstoArray_2460.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+class ApplyOperationstoArray_2460
+{
+public:
+    std::vector<int> applyOperations(std::vector<int> &nums)
+    {
+    }
+};
+int main()
+{
+    ApplyOperationstoArray_2460 a1;
+    std::vector<int> nums = {1, 2, 2, 1, 1, 0};
+    a1.applyOperations(nums);
+    return 0;
+}
