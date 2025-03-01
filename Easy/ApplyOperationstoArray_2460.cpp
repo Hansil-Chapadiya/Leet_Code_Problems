b04ad566543d402d1f3691c36b4a31ab2e9d93dd
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-03-01 22:46:13 +0530

    done with 100% beats

--- Easy/ApplyOperationstoArray_2460.cpp
+++ Easy/ApplyOperationstoArray_2460.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+/* #include <iostream>
 #include <vector>
 class ApplyOperationstoArray_2460
 {
@@ -40,6 +40,58 @@ int main()
     ApplyOperationstoArray_2460 a1;
     std::vector<int> nums = {1, 2, 2, 1, 1, 0};
     // std::vector<int> nums = {0, 1};
+
+    std::vector<int> answer = a1.applyOperations(nums);
+    for (auto i : answer)
+    {
+        std::cout << i << " ";
+    }
+    return 0;
+} */
+
+
+#include <iostream>
+#include <vector>
+
+class ApplyOperationstoArray_2460
+{
+public:
+    std::vector<int> applyOperations(std::vector<int> &nums)
+    {
+        // Step 1: Apply the operations
+        for (int i = 1; i < nums.size(); i++)
+        {
+            if (nums[i - 1] == nums[i])
+            {
+                nums[i - 1] *= 2;
+                nums[i] = 0;
+            }
+        }
+
+        // Step 2: Move all non-zero numbers forward
+        int cnt = 0; // Count of non-zero elements
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] != 0)
+            {
+                nums[cnt++] = nums[i];
+            }
+        }
+
+        // Step 3: Fill the remaining elements with zeroes
+        for (int i = cnt; i < nums.size(); i++)
+        {
+            nums[i] = 0; // FIXED: removed `i++`
+        }
+
+        return nums;
+    }
+};
+
+int main()
+{
+    ApplyOperationstoArray_2460 a1;
+    std::vector<int> nums = {1, 2, 2, 1, 1, 0};
 
     std::vector<int> answer = a1.applyOperations(nums);
     for (auto i : answer)
