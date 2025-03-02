682ae5fb79053394170964a7b63fc36124896343
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-03-02 08:53:49 +0530

    Easy/MergeTwo2DArrayBySummingValue_2570.py template is ready

---
+++ Easy/MergeTwo2DArrayBySummingValue_2570.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class MergeTwo2DArraysBySummingValues_2570:
+    def mergeArrays(
+        self, nums1: List[List[int]], nums2: List[List[int]]
+    ) -> List[List[int]]:
+        return []
+
+
+obj = MergeTwo2DArraysBySummingValues_2570()
+obj.mergeArrays([], [])
