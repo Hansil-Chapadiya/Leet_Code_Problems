511accfd1fb5789c59e9eaca637acb5b32193a20
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-03-03 08:58:22 +0530

    template is ready

---
+++ Medium/PartitionArrayAccordingtoGivenPivot_2161.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+class PartitionArrayAccordingtoGivenPivot
+{
+public:
+    std::vector<int> pivotArray(std::vector<int> &nums, int pivot)
+    {
+    }
+};
+int main()
+{
+    PartitionArrayAccordingtoGivenPivot p1;
+    std::vector<int> nums = {9, 12, 5, 10, 14, 3, 10};
+    p1.pivotArray(nums, 10);
+    return 0;
+}
