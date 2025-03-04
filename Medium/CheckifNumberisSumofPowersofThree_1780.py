b9badeb6747981cdaf5e6eeb3b509ccca84dbe67
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-03-04 08:50:49 +0530

    Medium/CheckifNumberisSumofPowersofThree_1780.py doen

--- Medium/CheckifNumberisSumofPowersofThree_1780.py
+++ Medium/CheckifNumberisSumofPowersofThree_1780.py
@@ -1,7 +1,13 @@
 class CheckIfNumberISSumOfPowersOfThree:
     def checkPowersOfThree(self, n: int) -> bool:
+        while n > 0:
+            rem = n % 3
+            if rem == 2:
+                return False
+            n //= 3
+
         return True
 
 
 obj = CheckIfNumberISSumOfPowersOfThree()
-print(obj.checkPowersOfThree(12))
+print(obj.checkPowersOfThree(10))
