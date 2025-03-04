2331d979ebd930926b6243cc084cc4b6663b205f
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-03-04 08:04:05 +0530

    template is ready

---
+++ Medium/CheckifNumberisSumofPowersofThree_1780.py
@@ -0,0 +1,7 @@
+class CheckIfNumberISSumOfPowersOfThree:
+    def checkPowersOfThree(self, n: int) -> bool:
+        return True
+
+
+obj = CheckIfNumberISSumOfPowersOfThree()
+print(obj.checkPowersOfThree(12))
