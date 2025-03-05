77796b50d78c342c23ce009735d25aacf806f15f
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-03-05 08:06:39 +0530

    template is ready

---
+++ Medium/CountTotalNumberOfColoredCells_2579.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+class CountTotalNumberOfColoredCells_2579
+{
+public:
+    long long coloredCells(int n)
+    {
+    }
+};
+int main()
+{
+    CountTotalNumberOfColoredCells_2579 c1;
+    c1.coloredCells(10);
+    return 0;
+}
