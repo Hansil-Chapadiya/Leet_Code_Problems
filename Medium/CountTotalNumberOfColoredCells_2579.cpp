b91d3aed932937b4e5ea4312367e7198f887b8df
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-03-05 09:12:33 +0530

    done

--- Medium/CountTotalNumberOfColoredCells_2579.cpp
+++ Medium/CountTotalNumberOfColoredCells_2579.cpp
@@ -1,15 +1,25 @@
 #include <iostream>
 #include <vector>
-class CountTotalNumberOfColoredCells_2579
+class CountTotalNumberofColoredCells_2579
 {
 public:
     long long coloredCells(int n)
     {
+        if (n == 1)
+            return 1;
+        long long ans = 1;
+        int count = 1;
+        for (int i = 1; i < n; i++)
+        {
+            ans += (count * 4);
+            count++;
+        }
+        return ans;
     }
 };
 int main()
 {
-    CountTotalNumberOfColoredCells_2579 c1;
-    c1.coloredCells(10);
+    CountTotalNumberofColoredCells_2579 c1;
+    std::cout << c1.coloredCells(4);
     return 0;
 }
