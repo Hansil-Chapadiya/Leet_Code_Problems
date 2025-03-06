4d694741cc0cc32d15ca153f4a221ebe91a6d017
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-03-06 17:38:02 +0530

    alternative approach

--- Easy/SingleNumber_136.py
+++ Easy/SingleNumber_136.py
@@ -1,9 +1,16 @@
 from typing import List
 from functools import reduce
 import operator
+
+
 class SingleNumber_136:
     def singleNumber(self, nums: List[int]) -> int:
-        return reduce(operator.xor, nums)
+        # return reduce(operator.xor, nums)
+        res = 0
+        for i in nums:
+            res ^= i
+        return res
+
 
 obj = SingleNumber_136()
-print(obj.singleNumber([]))
+print(obj.singleNumber([2,2,1]))
