965b32542585fbe34691d450134217c6c32a0e43
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-03-06 07:37:58 +0530

    FindMissingAndRepeatedValues_2965.py template is ready

---
+++ Easy/FindMissingAndRepeatedValues_2965.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class FindMissingAndRepeatedValues_2965:
+    def findMissingAndRepeatedValues(self, grid: List[List[int]]) -> List[int]:
+        return []
+
+
+obj = FindMissingAndRepeatedValues_2965()
+print(obj.findMissingAndRepeatedValues([[]]))
