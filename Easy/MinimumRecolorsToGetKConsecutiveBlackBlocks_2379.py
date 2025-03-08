88ca620435c6d623b9df5dd4b605c4c3c81fb10a
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-03-08 08:58:13 +0530

    done Easy/MinimumRecolorsToGetKConsecutiveBlackBlocks_2379.py

--- Easy/MinimumRecolorsToGetKConsecutiveBlackBlocks_2379.py
+++ Easy/MinimumRecolorsToGetKConsecutiveBlackBlocks_2379.py
@@ -1,6 +1,17 @@
 class MinimumRecolorsToGetKConsecutiveBlackBlocks_2379:
     def minimumRecolors(self, blocks: str, k: int) -> int:
-        return 0
+        min_ = 2**100
+        n = len(blocks)
+        if n == k:
+            return blocks[:k].count("W")
+        for i in range(n - k + 1):
+            subStr_ = blocks[i : i + k]
+            subCnt = subStr_.count("W")
+            min_ = min(min_, subCnt)
+
+        return min_
+
 
 obj = MinimumRecolorsToGetKConsecutiveBlackBlocks_2379()
-print(obj.minimumRecolors("",2))
+# print(obj.minimumRecolors("WBBWWBBWBW",7))
+print(obj.minimumRecolors("WBWBBBW", 2))
