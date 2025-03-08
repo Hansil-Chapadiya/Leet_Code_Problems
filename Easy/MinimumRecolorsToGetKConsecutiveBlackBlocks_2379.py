fbbf527ba137f9efc92fb5cc7a859dc7e66c3b0e
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-03-08 08:16:40 +0530

    template is ready

---
+++ Easy/MinimumRecolorsToGetKConsecutiveBlackBlocks_2379.py
@@ -0,0 +1,6 @@
+class MinimumRecolorsToGetKConsecutiveBlackBlocks_2379:
+    def minimumRecolors(self, blocks: str, k: int) -> int:
+        return 0
+
+obj = MinimumRecolorsToGetKConsecutiveBlackBlocks_2379()
+print(obj.minimumRecolors("",2))
