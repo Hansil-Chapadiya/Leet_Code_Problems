989f1b1e857553fb82c6bf06fb00a53c8dd54a8d
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-03-09 09:35:32 +0530

    TLE approach added

--- Medium/AlternatingGroupsII_3208.cpp
+++ Medium/AlternatingGroupsII_3208.cpp
@@ -1,17 +1,58 @@
 #include <iostream>
 #include <vector>
+
 class AlternatingGroupsII_3208
 {
 public:
     int numberOfAlternatingGroups(std::vector<int> &colors, int k)
     {
-        return 0;
+        int n = colors.size();
+        int index = 0;
+        if (n < k)
+        {
+            return 0;
+        }
+        for (int i = 1; i < n; i++)
+        {
+            if (i == colors.size() - 1 && colors[n - 1] == colors[0])
+            {
+                index = 0;
+                break;
+            }
+            if (colors[i - 1] == colors[i])
+            {
+
+                index = i;
+                break;
+            }
+        }
+
+        int cnt = 0;
+        for (int i = index; i < n; ++i)
+        {
+            bool alternating = true;
+            for (int j = 1; j < k; ++j)
+            {
+                if (colors[(i + j - 1) % n] == colors[(i + j) % n])
+                {
+                    alternating = false;
+                    break;
+                }
+            }
+            if (alternating)
+            {
+                cnt++;
+            }
+        }
+        return cnt;
     }
 };
+
 int main()
 {
     AlternatingGroupsII_3208 a1;
-    std::vector<int> colors = {0, 1, 0, 1, 0};
-    a1.numberOfAlternatingGroups(colors, 3);
+    // std::vector<int> colors = {0, 1, 0, 1, 0};
+    std::vector<int> colors = {0, 1, 0, 0, 1, 0, 1};
+    std::cout << a1.numberOfAlternatingGroups(colors, 6) << std::endl;
     return 0;
 }
