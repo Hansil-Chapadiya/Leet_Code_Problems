0aad991c84f2c2f753e67f1b7a7b5f6121c9cc56
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-03-09 08:17:58 +0530

    template is ready

---
+++ Medium/AlternatingGroupsII_3208.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+class AlternatingGroupsII_3208
+{
+public:
+    int numberOfAlternatingGroups(std::vector<int> &colors, int k)
+    {
+        return 0;
+    }
+};
+int main()
+{
+    AlternatingGroupsII_3208 a1;
+    std::vector<int> colors = {0, 1, 0, 1, 0};
+    a1.numberOfAlternatingGroups(colors, 3);
+    return 0;
+}
