5fd13f67ceadfea5fbb721e12edb4f36b0ecd771
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-03-10 08:24:12 +0530

    template is ready

---
+++ Medium/CountofSubstringsContainingEveryVowelAndKConsonantsII_3306.py
@@ -0,0 +1,7 @@
+class CountofSubstringsContainingEveryVowelAndKConsonantsII_3306:
+    def countOfSubstrings(self, word: str, k: int) -> int:
+        return 1
+
+
+obj = CountofSubstringsContainingEveryVowelAndKConsonantsII_3306()
+print(obj.countOfSubstrings("", 1))
