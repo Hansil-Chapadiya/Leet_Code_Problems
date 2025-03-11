2e46f2b14b452d2adb1620fbc0885ce3c0b38be4
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-03-11 08:14:11 +0530

    template is ready

---
+++ Medium/NumberofSubstringsContainingAllThreeCharacters_1358.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+class NumberofSubstringsContainingAllThreeCharacters_1358
+{
+public:
+    int numberOfSubstrings(std::string s)
+    {
+        return 0;
+    }
+};
+
+int main()
+{
+    NumberofSubstringsContainingAllThreeCharacters_1358 n1;
+    std::cout << n1.numberOfSubstrings(" ");
+
+    return 0;
+}
