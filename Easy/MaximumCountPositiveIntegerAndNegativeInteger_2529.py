39ca0a23ae7bfc7a3f9fbd4f29d3c9432e8aa977
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-03-12 08:13:37 +0530

    template is ready

---
+++ Easy/MaximumCountPositiveIntegerAndNegativeInteger_2529.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class MaximumCountPositiveIntegerAndNegativeInteger_2529:
+    def maximumCount(self, nums: List[int]) -> int:
+        return 0
+
+
+obj = MaximumCountPositiveIntegerAndNegativeInteger_2529()
+print(obj.maximumCount([-2, -1, -1, 1, 2, 3]))
