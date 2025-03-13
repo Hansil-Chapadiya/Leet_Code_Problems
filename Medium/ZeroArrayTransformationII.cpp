f819a3101bb65b1bf9d37212cccffd746e5cb7b4
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-03-13 08:12:07 +0530

    template is ready

---
+++ Medium/ZeroArrayTransformationII.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+class ZeroArrayTransformationII
+{
+public:
+    int minZeroArray(std::vector<int> &nums, std::vector<std::vector<int>> &queries)
+    {
+        return 0;
+    }
+};
+
+int main()
+{
+    ZeroArrayTransformationII z1;
+    std::vector<int> nums = {};
+    std::vector<std::vector<int>> queries = {};
+    z1.minZeroArray(nums, queries);
+
+    return 0;
+}
