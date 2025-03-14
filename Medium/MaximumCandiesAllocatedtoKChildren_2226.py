f4e055bf8438567563eba8922f3810ebee920ea0
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-03-14 08:41:40 +0530

    Medium/MaximumCandiesAllocatedtoKChildren_2226.py template is ready

---
+++ Medium/MaximumCandiesAllocatedtoKChildren_2226.py
@@ -0,0 +1,7 @@
+from typing import List
+class MaximumCandiesAllocatedtoKChildren_2226:
+    def maximumCandies(self, candies: List[int], k: int) -> int:
+        return 0
+
+obj = MaximumCandiesAllocatedtoKChildren_2226()
+print(obj.maximumCandies([],3))
