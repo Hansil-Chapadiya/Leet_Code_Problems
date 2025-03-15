2ddd780256c6671fff7901d6b3da2fffd17db514
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-03-15 09:31:55 +0530

    done

--- Medium/HouseRobberIV_2560.cpp
+++ Medium/HouseRobberIV_2560.cpp
@@ -1,17 +1,54 @@
 #include <iostream>
 #include <vector>
-class HouseRobberIV_2560
+#include <algorithm>
+
+class HouseRobberIV
 {
 public:
+    std::vector<int> nums;
+    int k;
+
+    bool isPossible(int mid)
+    {
+        int localK = 0;
+        int n = nums.size();
+        for (int i = 0; i < n; i++)
+        {
+            if (nums[i] <= mid)
+            {
+                localK++;
+                i++; // Skip the NEXT house ONLY after robbing
+            }
+        }
+        return localK >= k;
+    }
+
     int minCapability(std::vector<int> &nums, int k)
     {
-        return 0;
+        this->nums = nums;
+        this->k = k;
+        int highElement = *std::max_element(nums.begin(), nums.end());
+        int lowElement = *std::min_element(nums.begin(), nums.end());
+
+        while (lowElement <= highElement)
+        {
+            int mid = (lowElement + highElement) / 2;
+            if (isPossible(mid))
+            {
+                highElement = mid - 1;
+            }
+            else
+            {
+                lowElement = mid + 1;
+            }
+        }
+        return lowElement;
     }
 };
 
 int main()
 {
-    HouseRobberIV_2560 h1;
+    HouseRobberIV h1;
     std::vector<int> nums = {2, 3, 5, 9};
     std::cout << h1.minCapability(nums, 2);
     return 0;
