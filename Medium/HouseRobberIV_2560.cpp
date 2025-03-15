543e21c27382c37166bbf9df9bedc2b42df982ec
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-03-15 08:27:43 +0530

    template is readyt

---
+++ Medium/HouseRobberIV_2560.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+class HouseRobberIV_2560
+{
+public:
+    int minCapability(std::vector<int> &nums, int k)
+    {
+        return 0;
+    }
+};
+
+int main()
+{
+    HouseRobberIV_2560 h1;
+    std::vector<int> nums = {2, 3, 5, 9};
+    std::cout << h1.minCapability(nums, 2);
+    return 0;
+}
