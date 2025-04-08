ffe1d1832328682ffd1aafd35664456eac8cbf38
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-04-08 21:30:43 +0530

    Easy/MinimumNumberofOperationstoMakeElementsinArrayDistinct_3396.cpp another approach

--- Easy/MinimumNumberofOperationstoMakeElementsinArrayDistinct_3396.cpp
+++ Easy/MinimumNumberofOperationstoMakeElementsinArrayDistinct_3396.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+/* #include <iostream>
 #include <vector>
 #include <unordered_map>
 
@@ -55,6 +55,51 @@ int main()
     // std::vector<int> nums = {4,5,6,4,4};
     std::vector<int> nums = {1, 2, 3, 4, 2, 3, 3, 5, 7};
     std::cout << m1.minimumOperations(nums);
+
+    return 0;
+} */
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+class MinimumNumberofOperationstoMakeElementsinArrayDistinct_3396
+{
+public:
+    bool isValid(std::vector<int> arr)
+    {
+        std::unordered_map<int, int> freqmap;
+        for (int i = 0; i < arr.size(); i++)
+        {
+            freqmap[arr[i]]++;
+            if (freqmap[arr[i]] > 1)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    int minimumOperations(std::vector<int> &nums)
+    {
+        int cnt = 0;
+        while (!isValid(nums))
+        {
+            cnt++;
+            int removeCount = std::min(3, (int)nums.size());
+            nums.erase(nums.begin(), nums.begin() + removeCount);
+        }
+        return cnt;
+    }
+};
+
+int main()
+{
+    MinimumNumberofOperationstoMakeElementsinArrayDistinct_3396 m1;
+    std::vector<int> nums = {6, 7, 8, 9};
+    // std::vector<int> nums = {4, 5, 6, 4, 4};
+    // std::vector<int> nums = {1, 2, 3, 4, 2, 3, 3, 5, 7};
+    std::cout << m1.minimumOperations(nums) << std::endl;
 
     return 0;
 }
