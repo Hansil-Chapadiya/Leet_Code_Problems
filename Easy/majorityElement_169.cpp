a6553061ba3de3fc8e7085963b5061a17b4107e4
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-04-22 08:30:55 +0530

    sorting approach implemented

--- Easy/majorityElement_169.cpp
+++ Easy/majorityElement_169.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include <unordered_map>
 #include <algorithm>
-class Solution {
+/* class Solution {
     public:
         int majorityElement(std::vector<int>& nums) {
 
@@ -25,6 +25,44 @@ class Solution {
                     majorityElement = entry.first;
                     majorityCount = entry.second;
                 }
+            }
+            return majorityElement;
+
+        }
+    };
+    static const int hansil = []()
+    {
+        std::ios::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+        std::cout.tie(nullptr);
+        return 0;
+    }(); */
+
+class Solution {
+    public:
+        int majorityElement(std::vector<int>& nums) {
+
+            std::sort(nums.begin(), nums.end());
+            int max = 0;
+            int majorityElement = nums[0];
+            int condition = nums.size() / 2;
+
+            for (int i = 1; i < nums.size(); i++)
+            {
+                int cnt = 1;
+
+                while ( i < nums.size() && nums[i - 1] == nums[i])
+                {
+                    cnt++;
+                    i++;
+                }
+
+                if(cnt > max && cnt > condition )
+                {
+                    max = cnt;
+                    majorityElement = nums[i-1];
+                }
+
             }
             return majorityElement;
 
