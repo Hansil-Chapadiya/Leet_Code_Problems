8577e1b3c7cd977fa8396ae67e94fd068278e910
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-04-30 11:44:17 +0530

    Easy/FindNumberswithEvenNumberofDigits_1295.py maths formula version

--- Easy/FindNumberswithEvenNumberofDigits_1295.py
+++ Easy/FindNumberswithEvenNumberofDigits_1295.py
@@ -1,4 +1,4 @@
-from typing import List
+""" from typing import List
 
 class FindNumberswithEvenNumberofDigits_1295:
     def findNumbers(self, nums: List[int]) -> int:
@@ -6,6 +6,27 @@ class FindNumberswithEvenNumberofDigits_1295:
         for i in nums:
             nLen = len(str(i))
             if nLen % 2 == 0:
+                cnt += 1
+
+        return cnt
+
+obj = FindNumberswithEvenNumberofDigits_1295()
+print(obj.findNumbers([12,345,2,6,7896])) """
+
+from typing import List
+
+class FindNumberswithEvenNumberofDigits_1295:
+    def lenofN(self,n : int) -> int:
+        cnt = 0
+        while n > 0:
+            cnt += 1
+            n /= 10
+        return cnt
+
+    def findNumbers(self, nums: List[int]) -> int:
+        cnt  = 0
+        for i in nums:
+            if self.lenofN(i) % 2 == 0:
                 cnt += 1
 
         return cnt
