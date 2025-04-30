4de4871c094169b49856ae286a8046d5405be330
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-04-30 08:27:52 +0530

    solve with set

--- Easy/KeyBoardRow_500.cpp
+++ Easy/KeyBoardRow_500.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+/* #include <iostream>
 #include <vector>
 #include <unordered_set>
 #include <string>
@@ -68,4 +68,88 @@ static const int hansil = []()
     std::cin.tie(nullptr);
     std::cout.tie(nullptr);
     return 0;
-}();
+}(); */
+
+
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+#include <string>
+
+class KeyBoardRow_500
+{
+  private:
+    std::unordered_set<char> firstRow=  {'q','w','e','r','t','y','u','i','o','p'};
+    std::unordered_set<char> secondRow = {'a','s','d','f','g','h','j','k','l'};
+    std::unordered_set<char> thirdRow = {'x','c','v','b','m','n','z'};
+    std::vector<std::string> answer;
+  public:
+    std::vector<std::string> findWords(std::vector<std::string>& words)
+    {
+        for(auto s : words)
+        {
+            int sLen = s.length();
+            int cnt = 0;
+            for (auto c : s)
+            {
+                if(firstRow.find(std::tolower(c)) != firstRow.end())
+                {
+                    cnt++;
+                }
+
+            }
+            if(cnt == sLen)
+            {
+                answer.push_back(s);
+                continue;
+            }
+
+            cnt = 0;
+            for (auto c : s)
+            {
+                if(secondRow.find(std::tolower(c)) != secondRow.end())
+                {
+                    cnt++;
+                }
+
+            }
+            if(cnt == sLen)
+            {
+                answer.push_back(s);
+                continue;
+            }
+
+            cnt = 0;
+            for (auto c : s)
+            {
+                if(thirdRow.find(std::tolower(c)) != thirdRow.end())
+                {
+                    cnt++;
+                }
+
+            }
+            if(cnt == sLen)
+            {
+                answer.push_back(s);
+                continue;
+            }
+        }
+        return answer;
+    }
+};
+
+
+int main()
+{
+    std::vector<std::string> words = {"Hello","Alaska","Dad","Peace"};
+
+    KeyBoardRow_500 k1;
+    std::vector<std::string> answer = k1.findWords(words);
+
+    for(auto s : answer)
+    {
+        std::cout<<s<<" ";
+    }
+
+    return 0;
+}
