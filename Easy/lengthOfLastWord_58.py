191686e2b2150fd3fbaaa90795848a864ac58576
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-04-30 12:03:55 +0530

    Easy/lengthOfLastWord_58.py done with while loop approach

--- Easy/lengthOfLastWord_58.py
+++ Easy/lengthOfLastWord_58.py
@@ -1,4 +1,4 @@
-class Solution:
+""" class Solution:
     def lengthOfLastWord(self, s: str) -> int:
         n = len(s) - 1
 
@@ -17,3 +17,28 @@ class Solution:
                 cnt += 1
 
         return cnt
+ """
+
+class LengthOfLastWord_58:
+    def lengthOfLastWord(self, s: str) -> int:
+        n = len(s) - 1
+
+        # Skip trailing spaces
+        while n >= 0 and s[n] == " ":
+            n -= 1
+
+        # If string is only spaces
+        if n < 0:
+            return 0
+
+        cnt = 0
+        # Count length of last word
+        while n >= 0 and s[n] != ' ':
+            cnt += 1
+            n -= 1
+
+        return cnt
+
+# Test
+obj = LengthOfLastWord_58()
+print(obj.lengthOfLastWord("Hello World"))  # Output: 5
