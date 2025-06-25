bc553108ad52f04158c32930f1dfe523e9455451
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-06-25 22:51:10 +0530

    done using brute-force approach

---
+++ CodingTutotrial/stockBuyAndSell.py
@@ -0,0 +1,15 @@
+from typing import List
+
+class stockBuyAndSell:
+    def buyAndSell(self, nums: List[int]) -> int:
+        maxProfit = 0
+
+        for price in range(0,len(nums)):
+            minPrice = nums[0]
+            for priceCmp in range(price+1, len(nums)):
+                maxProfit = max(maxProfit, nums[priceCmp] - nums[price])
+        return maxProfit
+
+obj = stockBuyAndSell()
+# print(obj.buyAndSell([7,1,5,3,6,4]))
+print(obj.buyAndSell([7,6,4,3,1]))
