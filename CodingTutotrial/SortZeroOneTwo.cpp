e66ae1e8375e848eb971307c67a6cd801a01d352
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-06-26 09:00:16 +0530

    Using Dutch national flag approach

--- CodingTutotrial/SortZeroOneTwo.cpp
+++ CodingTutotrial/SortZeroOneTwo.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+/* #include<iostream>
 #include<vector>
 
 class SortZeroOneTwo
@@ -53,6 +53,60 @@ int main()
     SortZeroOneTwo obj;
 
     std::vector<int> arr = {2,0,2,1,1,0};
+    std::vector<int> answer = obj.sorting(arr);
+
+    for (auto i : answer)
+    {
+        std::cout<<i<<" ";
+    }
+
+    return 0;
+}
+
+static const int hansil = []()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return 0;
+}(); */
+
+
+#include<iostream>
+#include<vector>
+
+class SortZeroOneTwo
+{
+    public:
+    std::vector<int> sorting(std::vector<int> nums)
+    {
+        int zeroIndex = 0, oneIndex = 0, twoIndex = nums.size()-1;
+
+        while(oneIndex <= twoIndex)
+        {
+            if(nums[oneIndex] == 0)
+            {
+                std::swap(nums[oneIndex++], nums[zeroIndex++]);
+            }
+            else if(nums[oneIndex] == 1)
+            {
+                oneIndex++;
+            }
+            else
+            {
+                std::swap(nums[oneIndex++], nums[twoIndex--]);
+            }
+        }
+        return nums;
+    }
+};
+
+int main()
+{
+    SortZeroOneTwo obj;
+
+    // std::vector<int> arr = {2,0,2,1,1,0};
+    std::vector<int> arr = {0,0,0,0,1,1};
     std::vector<int> answer = obj.sorting(arr);
 
     for (auto i : answer)
