8090a779fbfc90df95fb0553182c5d5ec013e24c
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-06-27 17:45:05 +0530

    done next permutation

--- CodingTutotrial/nextPermutation.cpp
+++ CodingTutotrial/nextPermutation.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+/* #include <iostream>
 #include <vector>
 #include <algorithm>
 #include <functional>
@@ -45,6 +45,49 @@ int main()
             std::cout << num << " ";
         }
         std::cout << std::endl;
+    }
+
+    return 0;
+}
+ */
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <functional>
+
+class Permutation
+{
+public:
+    void nextPermutation(std::vector<int> &nums)
+    {
+        int n = nums.size();
+        int i = n - 2;
+
+        while(i >=0 && nums[i]>=nums[i+1]) i--;
+
+        if(i>=0)
+        {
+            int j = n - 1;
+            while(nums[j] <= nums[i]) j--;
+
+            std::swap(nums[i], nums[j]);
+        }
+
+        std::reverse(nums.begin() + i + 1, nums.end());
+    }
+};
+
+int main()
+{
+    Permutation obj;
+
+    std::vector<int> nums = {2, 1, 3};
+    obj.nextPermutation(nums);
+
+    for(int n: nums)
+    {
+        std::cout<<n<<" ";
     }
 
     return 0;
