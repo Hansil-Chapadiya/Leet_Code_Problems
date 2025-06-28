a34aa894aa61df0c403262d4465d923f7a4bddaf
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-06-28 14:04:55 +0530

    Using Binary Search

--- CodingTutotrial/FindDuplicate.py
+++ CodingTutotrial/FindDuplicate.py
@@ -1,4 +1,4 @@
-from typing import List
+""" from typing import List
 
 class FindDuplicate:
     def getDuplicateElement(self, nums: List[int]) -> int:
@@ -10,8 +10,50 @@ class FindDuplicate:
             if hashMap[num] > 1:
                 return num
 
-        return None
+        return -1
 
 obj = FindDuplicate()
 # print(obj.getDuplicateElement([1,3,4,2,2]))
-print(obj.getDuplicateElement([3,1,3,4,2]))
+print(obj.getDuplicateElement([3,1,3,4,2])) """
+
+
+f""" rom typing import List
+from collections import Counter
+
+class FindDuplicate:
+    def getDuplicateElement(self, nums: List[int]) -> int:
+
+        freqcounter = Counter(nums)
+        for item, count in freqcounter.items():
+            if count > 1:
+                return item
+
+        return -1
+
+obj = FindDuplicate()
+# print(obj.getDuplicateElement([1,3,4,2,2]))
+print(obj.getDuplicateElement([3,1,3,4,2])) """
+
+from typing import List
+from collections import Counter
+
+class FindDuplicate:
+    def getDuplicateElement(self, nums: List[int]) -> int:
+        low = 1
+        high = len(nums) - 1  # max value should be n-1
+
+        while low < high:
+            mid = (low + high) // 2
+            count = sum(num <= mid for num in nums)
+
+            if count > mid:
+                high = mid
+            else:
+                low = mid + 1
+
+        return low
+
+
+obj = FindDuplicate()
+print(obj.getDuplicateElement([1,3,4,2,2]))
+# print(obj.getDuplicateElement([3,1,3,4,2])) #1,2,3,3,4
