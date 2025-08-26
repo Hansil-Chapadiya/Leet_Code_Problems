4722fee06d5ac8fdf2fd597d626fdfd91822f5d9
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-08-26 11:38:46 +0530

    Easy/MaximumAreaofLongestDiagonalRectangle.cpp done

---
+++ Easy/MaximumAreaofLongestDiagonalRectangle.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+class MaximumAreaofLongestDiagonalRectangle
+{
+public:
+    int areaOfMaxDiagonal(std::vector<std::vector<int>> &dimensions)
+    {
+        //   int m = dimensions.length();
+        //   int n = 2;
+
+        float dgl = 0.0f;
+        int maxArea = 0;
+
+        for (auto dimension : dimensions)
+        {
+            float digonallength = std::sqrt(std::pow(dimension[0], 2) + std::pow(dimension[1], 2));
+            int area = dimension[0] * dimension[1];
+
+            if (digonallength > dgl)
+            {
+                dgl = digonallength;
+                maxArea = area;
+            }
+            else if (std::fabs(digonallength - dgl) && area > maxArea)
+            {
+                maxArea = area;
+            }
+        }
+
+        return maxArea;
+    }
+};
+
+int main()
+{
+    MaximumAreaofLongestDiagonalRectangle m1;
+
+    // std::vector<std::vector<int>> dimensions = {{9,3},{8,6}};
+    std::vector<std::vector<int>> dimensions = {{3, 4}, {4, 3}};
+    std::cout << m1.areaOfMaxDiagonal(dimensions);
+
+    return 0;
+}
